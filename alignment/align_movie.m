81d7106c02d6c615c2348a06663d0a56c758b1f5
kimth <kimth@stanford.edu>
2016-01-08 19:51:50 -0800

    Optional filling in of missing data

--- alignment/align_movie.m
+++ alignment/align_movie.m
@@ -3,7 +3,7 @@ function M_aligned = align_movie(M_image, M_pos)
 M_aligned = zeros(size(M_image));
 num_frames = size(M_aligned, 3);
 
-% Special handling for the first frame
+% Special handling for the first frame, which we assume is ideally aligned
 [frame, align_info] = align_lines(M_image(:,:,1), M_pos(:,:,1));
 M_aligned(:,:,1) = frame;
 
@@ -15,9 +15,39 @@ for k = 2:num_frames
     frame = M_image(:,:,k);
     pos_data = M_pos(:,:,k);
     
-    % Correct for galvo nonlinearity
-    frame =  align_lines(frame, pos_data, 'ref', align_info);
+    % Correct for galvo drift
+    frame = align_lines(frame, pos_data, 'ref', align_info);
+    
+    % (Optional) Fill in missing values
+    frame = fill_frame(frame);
     
     % Store results
     M_aligned(:,:,k) = frame;
+end
+
+end % align_movie
+
+function frame = fill_frame(frame)
+% Look for missing (negative) pixel values in the frame, and replace with
+% pixel values taken from adjacent lines
+
+num_lines = size(frame, 1);
+
+% First line
+line = frame(1,:);
+pixels_to_replace = find(line < 0);
+frame(1, pixels_to_replace) = frame(2, pixels_to_replace);
+
+for k = 2:(num_lines-1)
+    line = frame(k,:);
+    pixels_to_replace = find(line < 0);
+    frame(k, pixels_to_replace) = mean(...
+        [frame(k-1, pixels_to_replace); frame(k+1, pixels_to_replace)]);
+end
+
+% Last line
+line = frame(num_lines,:);
+pixels_to_replace = find(line < 0);
+frame(num_lines, pixels_to_replace) = frame(num_lines-1, pixels_to_replace);
+
 end
