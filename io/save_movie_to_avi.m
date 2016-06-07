18a1f190a86d309b602755a427baada18f0cc362
kimth <kimth@stanford.edu>
2016-06-07 09:05:10 -0700

    save_movie_to_avi finally makes it as a function

---
+++ io/save_movie_to_avi.m
@@ -0,0 +1,22 @@
+function save_movie_to_avi(M, frame_rate)
+
+switch class(M)
+    case {'uint16'}
+        scale = [0 0.9*max(M(:))];
+    case {'single'}
+        scale = compute_movie_scale(M);
+end
+
+writerObj = VideoWriter('out.avi', 'Uncompressed AVI');
+writerObj.FrameRate = frame_rate;
+open(writerObj);
+for i = 1:size(M,3)
+    m = M(:,:,i);
+    imagesc(m, scale);
+    axis image;
+    truesize;
+    colormap gray;
+    set(gca,'Visible','Off');
+    writeVideo(writerObj, getframe);
+end
+close(writerObj);
