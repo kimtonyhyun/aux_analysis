cb399618e61b04811b39eefbc76b2a207c62c7cb
kimth <kimth@stanford.edu>
2017-06-04 13:29:29 -0700

    NC reads tif

--- normcorre/run_normcorre.m
+++ normcorre/run_normcorre.m
@@ -1,15 +1,24 @@
 function run_normcorre(movie_in, movie_out)
 % Run NoRMCorre motion correction, with imaging data streamed from disk
-% TODO: Allow for TIF input
+%
+% NormCorre obtained from https://github.com/simonsfoundation/NoRMCorre
+%   on 2017 June 02 (latest commit f050cc1 / Apr 26).
 
+[~, name, ext] = fileparts(movie_in);
 if isempty(movie_out)
-    [~, name] = fileparts(movie_in);
     movie_out = sprintf('%s_nc.hdf5', name);
     fprintf('run_normcorre: Output movie will be saved as "%s"\n', movie_out);
 end
 
-% FIXME: Hard-coded NoRMCorre code to save data to '/Data/Images'
-[movie_size, ~] = get_dataset_info(movie_in, '/Data/Images');
+switch lower(ext)
+    case '.tif'
+        info = imfinfo(movie_in);
+        movie_size = [info.Height info.Width length(info)];
+    case {'.hdf5', '.h5'}
+        % FIXME: Hard-coded to save data to '/Data/Images'
+        [movie_size, ~] = get_dataset_info(movie_in, '/Data/Images');
+end
+
 options_nonrigid = NoRMCorreSetParms('d1',movie_size(1),'d2',movie_size(2),...
                     'grid_size',[64,64],...
                     'mot_uf',4,'bin_width',50,...
