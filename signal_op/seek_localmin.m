77bf2ee7cac48e25d1f8bfda14c885558cb926fa
kimth <kimth@stanford.edu>
2017-10-02 10:45:46 -0700

    Seek localmin

--- signal_op/seek_localmin.m
+++ signal_op/seek_localmin.m
@@ -1,23 +1,23 @@
-function x = seek_localmax(trace, x)
+function x = seek_localmin(trace, x)
 
 iter = 0;
 while (iter < 100) % Hard brake to prevent long/infinite loops
     if x == 1
-        delta_left = -Inf;
+        delta_left = Inf;
     else
         delta_left = trace(x-1) - trace(x);
     end
 
     if x == length(trace)
-        delta_right = -Inf; 
+        delta_right = Inf; 
     else
         delta_right = trace(x+1) - trace(x);
     end
 
-    if (delta_left <= 0) && (delta_right <= 0)
+    if (delta_left >= 0) && (delta_right >= 0)
         break;
     else
-        if delta_left > delta_right
+        if delta_left < delta_right
             x = x - 1;
         else
             x = x + 1;
