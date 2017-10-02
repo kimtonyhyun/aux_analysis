a2a767bf6a58f55c980f88ba03edbc9a058ba365
kimth <kimth@stanford.edu>
2017-10-01 17:51:07 -0700

    Find all events without regards to amplitude

--- events/find_events.m
+++ events/find_events.m
@@ -6,10 +6,26 @@ above_thresh_frames = find(trace > threshold);
 segments = frame_list_to_segments(above_thresh_frames);
 num_segments = size(segments,1);
 
-events = zeros(1,num_segments);
+events = cell(1,num_segments);
 for k = 1:num_segments
     seg = segments(k,1):segments(k,2);
     tr_seg = trace(seg);
-    [~, max_ind] = max(tr_seg);
-    events(k) = seg(max_ind);
+    
+%     events{k} = find_max(seg, tr_seg);
+    events{k} = find_all_localmax(seg, tr_seg);
 end
+events = cell2mat(events);
+
+end % find_events
+
+function event = find_max(seg, tr_seg)
+    [~, max_ind] = max(tr_seg);
+    event = seg(max_ind);
+end % find_max
+
+function events = find_all_localmax(seg, tr_seg)
+    x1 = [false tr_seg(2:end)>tr_seg(1:end-1)];
+    x2 = [tr_seg(1:end-1)>tr_seg(2:end) false];
+
+    events = seg(x1 & x2);
+end % find_all_localmax
