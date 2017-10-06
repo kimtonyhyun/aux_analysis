5a9ee28761a2dd428a439133f936f21ecaa4295e
kimth <kimth@stanford.edu>
2017-10-06 12:22:49 -0700

    Code reorg -- wip

--- events/detect_events_interactively.m
+++ events/detect_events_interactively.m
@@ -33,28 +33,28 @@ end
 
 % Basic trace properties
 num_frames = length(trace);
-trace_range = [min(trace) max(trace)];
-trace_range = trace_range + 0.1*diff(trace_range)*[-1 1];
+trace_display_range = [min(trace) max(trace)];
+trace_display_range = trace_display_range + 0.1*diff(trace_display_range)*[-1 1];
 [init_threshold, stats] = estimate_baseline_threshold(trace);
 
 % Application state
+state.allow_manual_events = false;
 state.x_anchor = 1;
-state.x_range = min(1000, num_frames);
-state.show_raw = true;
+state.x_range = min(500, num_frames);
+state.show_orig = true;
 state.show_dots = false;
 state.sel_event = 0;
 
-events.threshold = init_threshold;
-events.auto = find_events(trace, events.threshold);
-events.manual = [];
+events = struct('threshold', [], 'auto', [], 'manual', []);
 
 hfig = figure;
-gui = setup_gui(hfig, num_frames, trace_range, stats, trace_orig);
-redraw_threshold(gui);
+gui = setup_gui(hfig, num_frames, trace_display_range, stats, trace_orig);
+set_threshold(init_threshold, gui);
+update_gui_state(gui, state);
 
 % Interaction loop:
 %------------------------------------------------------------
-prompt = 'Event detector >> ';
+prompt = '  >> ';
 resp = lower(strtrim(input(prompt, 's')));
 val = str2double(resp);
 
@@ -80,25 +80,18 @@ while (1)
                 redraw_local_window(gui);
                 
             case 'r' % toggle raw trace
-                state.show_raw = ~state.show_raw;
-                if (state.show_raw)
-                    set(gui.local_raw, 'Visible', 'on');
-                else
-                    set(gui.local_raw, 'Visible', 'off');
-                end
+                state.show_orig = ~state.show_orig;
+                update_gui_state(gui, state);
                 
             case 'd' % show dots
                 state.show_dots = ~state.show_dots;
-                if (state.show_dots)
-                    set(gui.local_dots, 'Visible', 'on');
-                else
-                    set(gui.local_dots, 'Visible', 'off');
-                end
+                update_gui_state(gui, state);
                 
             case 't' % reset threshold
-                events.threshold = init_threshold;
-                events.auto = find_events(trace, events.threshold);
-                redraw_threshold(gui);
+                set_threshold(init_threshold, gui);
+                
+            case 'm' % toggle manual input
+                state.allow_manual_events = ~state.allow_manual_events;
                 
             case 'x' % erase last event
                 num_events = length(events.manual);
@@ -136,45 +129,49 @@ end % Main interaction loop
         redraw_local_window(gui);
     end % get_prev_page
 
-    function gui = setup_gui(hf, num_frames, trace_range, trace_stats, trace_orig)
+    function gui = setup_gui(hf, num_frames, trace_display_range, stats, trace_orig)
         % Display parameters kept around for convenience
         gui.num_frames = num_frames;
-        gui.trace_range = trace_range;
+        gui.trace_display_range = trace_display_range;
         
         % Setup the GLOBAL trace plot
+        %------------------------------------------------------------
         gui.global = subplot(2,4,1:3);
-        gui.global_rect = rectangle('Position',[state.x_anchor trace_range(1) state.x_range diff(trace_range)],...
+        gui.global_rect = rectangle('Position',[-1 trace_display_range(1) 1 diff(trace_display_range)],...
                   'EdgeColor', 'none',...
                   'FaceColor', 'c', 'HitTest', 'off');
         hold on;
         plot(trace, 'k', 'HitTest', 'off');
-        gui.global_thresh = plot([1 num_frames], events.threshold*[1 1], 'm--', 'HitTest', 'off');
+        gui.global_thresh = plot([1 num_frames], -Inf*[1 1], 'm--', 'HitTest', 'off');
         gui.global_auto = plot(-Inf, -1, 'm.', 'HitTest', 'off');
         gui.global_manual = plot(-Inf, -1, 'r.', 'HitTest', 'off');
         hold off;
         box on;
         xlim([1 num_frames]);
-        ylim(trace_range);
+        ylim(trace_display_range);
         xlabel('Frame');
-        ylabel('Fluorescence');
+        ylabel('Fluorescence (a.u.)');
         
         % Setup the HISTOGRAM plot
+        %------------------------------------------------------------
         gui.histogram = subplot(4,4,4);
-        semilogy(trace_stats.hist_centers, trace_stats.hist_counts, 'k.', 'HitTest', 'off');
-        xlim(trace_range);
+        semilogy(stats.hist_centers, stats.hist_counts, 'k.', 'HitTest', 'off');
+        xlim(trace_display_range);
         hold on;
-        count_range = [1 10^ceil(log10(max(trace_stats.hist_counts)))]; % First power of 10 that exceeds the maximum count
+        % First power of 10 that exceeds the maximum count
+        count_range = [1 10^ceil(log10(max(stats.hist_counts)))];
         ylim(count_range);
-        for k = 1:size(trace_stats.percentiles,1)
-            y = trace_stats.percentiles(k,2);
-            plot(y*[1 1], count_range, 'Color', 0.5*[1 1 1], 'HitTest', 'off');
+        for k = 1:size(stats.percentiles,1)
+            f = stats.percentiles(k,2);
+            plot(f*[1 1], count_range, 'Color', 0.5*[1 1 1], 'HitTest', 'off');
         end
-        gui.histogram_thresh = plot(events.threshold*[1 1], count_range, 'm--', 'HitTest', 'off');
+        gui.histogram_thresh = plot(-Inf*[1 1], count_range, 'm', 'HitTest', 'off');
         hold off;
         xlabel('Fluorescence');
         ylabel('Trace histogram');
 
         % Setup the event amplitude CDF
+        %------------------------------------------------------------
         gui.event_amp_cdf = subplot(4,4,8);
         gui.cdf = plot(-1, -1, 'm.-', 'HitTest', 'off');
         hold on;
@@ -182,39 +179,28 @@ end % Main interaction loop
         xlim([0 1]);
         ylim([0 1]);
         grid on;
-        xlabel('Norm amplitude');
-        ylabel('Event amplitude CDF');
+        xlabel('Norm event amplitude');
+        ylabel('CDF');
         
         % Setup the LOCAL trace plot
+        %------------------------------------------------------------
         gui.local = subplot(2,1,2);
-        gui.local_raw = plot(trace_orig, 'Color', 0.6*[1 1 1], 'HitTest', 'off');
+        gui.local_orig = plot(trace_orig, 'Color', 0.6*[1 1 1], 'HitTest', 'off');
         hold on;
         plot(trace, 'k', 'HitTest', 'off');
         gui.local_dots = plot(trace, 'k.', 'HitTest', 'off');
-        if state.show_raw
-            set(gui.local_raw, 'Visible', 'on');
-        else
-            set(gui.local_raw, 'Visible', 'off');
-        end
-        if state.show_dots
-            set(gui.local_dots, 'Visible', 'on');
-        else
-            set(gui.local_dots, 'Visible', 'off');
-        end
-        gui.local_dot = plot(-1,trace(1),'ro',...
+        gui.local_cursor_dot = plot(-1,trace(1),'ro',...
             'MarkerFaceColor','r',...
             'MarkerSize',6,'HitTest','off');
-        gui.local_bar = plot(-Inf*[1 1], trace_range, 'k--', 'HitTest', 'off');
-        gui.local_thresh = plot([1 num_frames], events.threshold*[1 1], 'm--', 'HitTest', 'off');
+        gui.local_cursor_bar = plot(-Inf*[1 1], trace_display_range, 'k--', 'HitTest', 'off');
+        gui.local_thresh = plot([1 num_frames], -Inf*[1 1], 'm--', 'HitTest', 'off');
         gui.local_auto = plot(-1, -1, 'm:');
-        gui.local_sel_event = plot([-1 -1], trace_range, 'm');
+        gui.local_sel_event = plot([-1 -1], trace_display_range, 'm');
         gui.local_auto_amps = plot(-1, -1, 'm', 'LineWidth', 2);
         gui.local_manual = plot(-1, -1, 'r');
         hold off;
-        ylim(trace_range);
+        ylim(trace_display_range);
         grid on;
-        x_range = [state.x_anchor, state.x_anchor+state.x_range-1];
-        xlim(x_range);
         xlabel('Frame');
         ylabel('Fluorescence');
         
@@ -228,12 +214,10 @@ end % Main interaction loop
         
         function track_cursor(~, e, gui)
             x = round(e.IntersectionPoint(1));
-            if ((state.x_anchor<=x)&&(x<=state.x_anchor+state.x_range))
-                if ((1<=x)&&(x<=gui.num_frames))                  
-                    x = seek_localmax(trace, x);
-                    set(gui.local_bar,'XData',x*[1 1]);
-                    set(gui.local_dot,'XData',x,'YData',trace(x));
-                end
+            if ((1<=x)&&(x<=gui.num_frames))                  
+                x = seek_localmax(trace, x);
+                set(gui.local_cursor_bar,'XData',x*[1 1]);
+                set(gui.local_cursor_dot,'XData',x,'YData',trace(x));
             end
         end % track_cursor
         
@@ -249,35 +233,52 @@ end % Main interaction loop
 
     % Update the GUI
     %------------------------------------------------------------
+    function update_gui_state(gui, state)
+        redraw_local_window(gui);
+        
+        if state.show_orig
+            set(gui.local_orig, 'Visible', 'on');
+        else
+            set(gui.local_orig, 'Visible', 'off');
+        end
+        if state.show_dots
+            set(gui.local_dots, 'Visible', 'on');
+        else
+            set(gui.local_dots, 'Visible', 'off');
+        end
+    end
+
     function redraw_local_window(gui)
         rect_pos = get(gui.global_rect, 'Position');
         rect_pos(1) = state.x_anchor;
         rect_pos(3) = state.x_range;
         set(gui.global_rect, 'Position', rect_pos);
-        
+               
         subplot(gui.local);
         xlim([state.x_anchor, state.x_anchor+state.x_range-1]);
-        
-
-    end % redraw_local_window
-
-    function redraw_threshold(gui)        
+    end
+    
+    function redraw_threshold(gui)
+        % GLOBAL subplot
         set(gui.global_thresh, 'YData', events.threshold*[1 1]);
         auto_peaks = events.auto(:,2)';
         set(gui.global_auto, 'XData', auto_peaks, 'YData', trace(auto_peaks));
         update_event_tally(gui);
         
+        % HISTOGRAM subplot
         set(gui.histogram_thresh, 'XData', events.threshold*[1 1]);
         
+        % CDF subplot
         [f,x] = ecdf(events.auto(:,3));
         set(gui.cdf, 'XData', x(2:end)/max(x), 'YData', f(2:end));
         
+        % LOCAL subplot
         set(gui.local_thresh, 'YData', events.threshold*[1 1]);
         
         % Note: NaN's break connections between line segments
         num_auto_events = size(events.auto, 1);
         X = kron(auto_peaks, [1 1 NaN]);
-        Y = repmat([gui.trace_range NaN], 1, num_auto_events);
+        Y = repmat([gui.trace_display_range NaN], 1, num_auto_events);
         set(gui.local_auto, 'XData', X, 'YData', Y);
         
         % Draw event amplitudes
@@ -292,7 +293,7 @@ end % Main interaction loop
         set(gui.global_manual, 'XData', events.manual, 'YData', trace(events.manual));
         
         X = kron(events.manual', [1 1 NaN]);
-        Y = repmat([gui.trace_range NaN], 1, length(events.manual));
+        Y = repmat([gui.trace_display_range NaN], 1, length(events.manual));
         set(gui.local_manual, 'XData', X, 'YData', Y);
         
         update_event_tally(gui);
@@ -314,16 +315,14 @@ end % Main interaction loop
                 x = round(e.IntersectionPoint(1));
                 if ((1<=x) && (x<=gui.num_frames))
                     state.x_anchor = x - state.x_range/2;
-                    redraw_local_window(gui);
+                    update_gui_state(gui, state);
                 else
                     fprintf('\n  Not a valid frame for this trace!\n');
                 end
                 
             case 3 % Right click -- Set threshold
                 t = e.IntersectionPoint(2);
-                events.threshold = t;
-                events.auto = find_events(trace, t);
-                redraw_threshold(gui);
+                set_threshold(t, gui);
         end
     end % global_plot_handler
 
@@ -331,9 +330,7 @@ end % Main interaction loop
         switch e.Button
             case 1 % Left click -- Set threshold
                 t = e.IntersectionPoint(1);
-                events.threshold = t;
-                events.auto = find_events(trace, t);
-                redraw_threshold(gui);
+                set_threshold(t, gui);
             case 3 % Right click
                 
         end
@@ -354,12 +351,8 @@ end % Main interaction loop
                 sel_frame = events.auto(se,2);
                 state.x_anchor = sel_frame - 1/2 * state.x_range;
                 
-                state.sel_event = se;
-                if (1 <= se) && (se <= size(events.auto,1))
-                    set(gui.cdf_sel_event, 'XData', event_amps(se)/amp_scale*[1 1]);
-                    set(gui.local_sel_event, 'XData', sel_frame*[1 1]);
-                end
-                redraw_local_window(gui);
+                select_event(se, gui);
+                update_gui_state(gui, state);
             case 3 % Right click
                 
         end
@@ -370,12 +363,16 @@ end % Main interaction loop
             case 1 % Left click
                 x = round(e.IntersectionPoint(1));
                 if ((1<=x) && (x<=gui.num_frames))
-                    x = seek_localmax(trace,x);
-                    % Don't make duplicate events
-                    auto_peaks = events.auto(:,2);
-                    if ~ismember(x, auto_peaks) && ~ismember(x, events.manual)
-                        events.manual = [events.manual; x];
-                        redraw_manual_events(gui);
+                    if state.allow_manual_events
+                        x = seek_localmax(trace,x);
+                        % Don't make duplicate events
+                        auto_peaks = events.auto(:,2);
+                        if ~ismember(x, auto_peaks) && ~ismember(x, events.manual)
+                            events.manual = [events.manual; x];
+                            redraw_manual_events(gui);
+                        end
+                    else
+
                     end
                 else
                     fprintf('\n  Not a valid event for this trace!\n');
@@ -386,4 +383,28 @@ end % Main interaction loop
         end
     end % local_plot_handler
 
+    % Data processing
+    %------------------------------------------------------------
+    function set_threshold(t, gui)
+        events.threshold = t;
+        events.auto = find_events(trace, t);
+        select_event(0, gui);
+        redraw_threshold(gui);
+    end % set_threshold
+
+    function select_event(se, gui)
+        if (0 <= se) && (se <= size(events.auto,1))
+            state.sel_event = se;
+            if (se > 0)
+                event_amps = events.auto(:,3);
+                sel_event_frame = events.auto(se,2);
+                sel_event_normamp = event_amps(se)/max(event_amps);
+            else
+                sel_event_frame = -Inf;
+                sel_event_normamp = -Inf;
+            end
+            set(gui.cdf_sel_event, 'XData', sel_event_normamp*[1 1]);
+            set(gui.local_sel_event, 'XData', sel_event_frame*[1 1]);
+        end
+    end
 end % detect_events_interactively
