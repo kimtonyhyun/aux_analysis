4840582ab36bc89759ffea27462ffbf080c1c123
kimth <kimth@stanford.edu>
2017-11-11 16:06:44 -0800

    Helper function to split data into training and test

--- logistic_regression/compute_logistic_test_scores.m
+++ logistic_regression/compute_logistic_test_scores.m
@@ -9,32 +9,15 @@ function test_scores = compute_logistic_test_scores(pre_features, post_features)
 num_runs = 10;
 training_frac = 0.7;
 
-num_pre = size(pre_features, 1);
-num_post = size(post_features, 1);
-
-num_pre_train = round(training_frac * num_pre);
-num_pre_test = num_pre - num_pre_train;
-num_post_train = round(training_frac * num_post);
-num_post_test = num_post - num_post_train;
-num_test = num_pre_test + num_post_test;
-
 test_scores = zeros(num_runs,1);
 for r = 1:num_runs
     % Get a random train/test split
-    pre_train = randperm(num_pre) <= num_pre_train;
-    pre_test = ~pre_train;
-    post_train = randperm(num_post) <= num_post_train;
-    post_test = ~post_train;
+    [train, test] = split_train_test(pre_features, post_features, training_frac);
     
     % Fit logistic regression to training data
-    f_train = [pre_features(pre_train,:); post_features(post_train,:)];
-    y_train = [zeros(1,num_pre_train) ones(1,num_post_train)]'; % Note: 0 <==> Pre, 1 <==> Post
-    w = fit_logistic_regression(f_train, y_train);
+    w = fit_logistic_regression(train.X, train.y);
     
     % Evaluate on training data
-    f_test = [pre_features(pre_test,:); post_features(post_test,:)];
-    y_test = [zeros(1,num_pre_test) ones(1,num_post_test)]';
-    y_pred = make_prediction(w, f_test);
-    
-    test_scores(r) = sum(y_pred == y_test) / num_test;
+    y_pred = make_prediction(w, test.X);
+    test_scores(r) = sum(y_pred == test.y) / test.n;
 end
---
+++ logistic_regression/split_train_test.m
@@ -0,0 +1,32 @@
+function [train, test] = split_train_test(pre_features, post_features, training_frac)
+% Split the data into training and testing sets at the desired training
+% fraction. Sample uniformly from PRE and POST examples.
+%
+% Format:
+%   pre_features: [num_pre_examples x num_features]
+%   post_features: [num_post_examples x num_features]
+%
+num_pre = size(pre_features, 1);
+num_post = size(post_features, 1);
+
+num_pre_train = round(training_frac * num_pre);
+num_pre_test = num_pre - num_pre_train;
+num_post_train = round(training_frac * num_post);
+num_post_test = num_post - num_post_train;
+
+% Get a random train/test split
+pre_train = randperm(num_pre) <= num_pre_train;
+pre_test = ~pre_train;
+post_train = randperm(num_post) <= num_post_train;
+post_test = ~post_train;
+
+% Format output as structs
+train = struct(...
+    'X', [pre_features(pre_train,:); post_features(post_train,:)],...
+    'y', [zeros(num_pre_train,1); ones(num_post_train,1)],...
+    'n', num_pre_train + num_post_train);
+
+test = struct(...
+    'X', [pre_features(pre_test,:); post_features(post_test,:)],...
+    'y', [zeros(num_pre_test,1); ones(num_post_test,1)],...
+    'n', num_pre_test + num_post_test);
