ccf9f78290cfdd4015b0c1b91fdeee077c304794
kimth <kimth@stanford.edu>
2017-11-13 20:16:42 -0800

    Return all logistic models and specify number of runs

--- logistic_regression/compute_logistic_test_scores.m
+++ logistic_regression/compute_logistic_test_scores.m
@@ -1,4 +1,4 @@
-function test_scores = compute_logistic_test_scores(pre_features, post_features)
+function [test_scores, W] = compute_logistic_test_scores(pre_features, post_features, num_runs)
 % Compute test scores multiple times by resampling training and test data
 %
 % Format:
@@ -6,10 +6,15 @@ function test_scores = compute_logistic_test_scores(pre_features, post_features)
 %   post_features: [num_examples x num_features]
 %
 
-num_runs = 20;
+if nargin < 3
+    num_runs = 20;
+end
+
+num_features = size(pre_features, 2);
 training_frac = 0.7;
 
 test_scores = zeros(num_runs,1);
+W = zeros(num_runs, num_features + 1);
 for r = 1:num_runs
     % Get a random train/test split
     [train, test] = split_train_test(pre_features, post_features, training_frac);
@@ -19,5 +24,7 @@ for r = 1:num_runs
     
     % Evaluate on training data
     y_pred = make_prediction(w, test.X);
+    
+    W(r,:) = w;
     test_scores(r) = sum(y_pred == test.y) / test.n;
 end
