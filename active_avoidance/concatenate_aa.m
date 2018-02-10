a1298a92fe010e867d80328f2f676264feb83932
kimth <kimth@stanford.edu>
2018-02-10 10:28:33 -0800

    Preprocessing AA data

--- active_avoidance/concatenate_aa.m
+++ active_avoidance/concatenate_aa.m
@@ -1,5 +1,10 @@
+clear all; close all;
+
 %% Load all data into a single matrix
 
+% Expected number of frames:
+%   - 8000 baseline frames prior to trials
+%   - 600 frames per trial, 100 trials
 M = zeros(512,512,8000+600*100,'int16');
 
 M_baseline = load_scanimage_tif('baseline_00001.tif');
@@ -8,7 +13,7 @@ M(:,:,1:8000) = M_baseline;
 idx = 8001;
 for k = 1:100
     fprintf('Loading Trial %d...\n', k);
-    filename = sprintf('baseline_%05d.tif', k+1);
+    filename = sprintf('trial_%05d.tif', k);
     M_trial = load_scanimage_tif(filename);
     M(:,:,idx:(idx+599)) = M_trial;
     idx = idx + 600;
@@ -16,27 +21,32 @@ end
 
 clear filename idx k M_baseline M_trial;
 
-%% Split into four separate movies
+%% Inspect movie for abnormalities (i.e. saturated PMT)
+F = compute_fluorescence_stats(M);
+plot(F);
+grid on;
+xlim([1 size(M,3)]);
+ylim([-500 2000]);
+xlabel('Frames');
+ylabel('Fluorescence');
 
-M1 = M(:,:,1:4:end);
-M2 = M(:,:,2:4:end);
-M3 = M(:,:,3:4:end);
-M4 = M(:,:,4:4:end);
+%% Examine the slices
 
-A1 = mean(M1,3);
-A2 = mean(M2,3);
-A3 = mean(M3,3);
-A4 = mean(M4,3);
+A1 = mean(M(:,:,1:4:end),3);
+A2 = mean(M(:,:,2:4:end),3);
+A3 = mean(M(:,:,3:4:end),3);
+A4 = mean(M(:,:,4:4:end),3);
 
+figure;
 subplot(221); imagesc(A1); axis image; colormap gray; title('Slice 1');
 subplot(222); imagesc(A2); axis image; title('Slice 2');
 subplot(223); imagesc(A3); axis image; title('Slice 3');
 subplot(224); imagesc(A4); axis image; title('Slice 4');
 
 %% Save sub-movies to file
-stem = 'm753-1020';
+stem = 'f761-0909';
 
-save_movie_to_hdf5(M1, sprintf('%s-sl1.hdf5', stem));
-save_movie_to_hdf5(M2, sprintf('%s-sl2.hdf5', stem));
-save_movie_to_hdf5(M3, sprintf('%s-sl3.hdf5', stem));
-% save_movie_to_hdf5(M4, sprintf('%s-sl4.hdf5', stem));
+save_movie_to_hdf5(M(:,:,1:4:end), sprintf('%s-sl1.hdf5', stem));
+save_movie_to_hdf5(M(:,:,2:4:end), sprintf('%s-sl2.hdf5', stem));
+save_movie_to_hdf5(M(:,:,3:4:end), sprintf('%s-sl3.hdf5', stem));
+% save_movie_to_hdf5(M(:,:,4:4:end), sprintf('%s-sl4.hdf5', stem));
---
+++ active_avoidance/register_aa.m
@@ -0,0 +1,21 @@
+stem = 'f761-0909';
+
+slices = [2 3 4];
+num_slices = length(slices);
+
+for sl = slices
+    filename_in = sprintf('%s-sl%d.hdf5', stem, sl);
+    filename_mc1 = sprintf('%s-sl%d_mc1.hdf5', stem, sl);
+    filename_mc2 = sprintf('%s-sl%d_mc2.hdf5', stem, sl);
+    
+    % Round 1 of TurboReg
+    M = load_movie(filename_in);
+    A = mean(M,3);
+    clear M;
+    register_movie(filename_in, filename_mc1, 'nofilter', 'noroi', 'ref', A);
+    
+    M = load_movie(filename_mc1);
+    A = mean(M,3);
+    clear M;
+    register_movie(filename_mc1, filename_mc2, 'nofilter', 'noroi', 'ref', A);
+end
