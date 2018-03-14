ae38a9e2770b69a8fc377dff4764405675ff2226
kimth <kimth@stanford.edu>
2018-03-14 10:55:52 -0700

    rescale_movie can also resize in space

--- image_op/rescale_movie.m
+++ image_op/rescale_movie.m
@@ -1,11 +1,34 @@
-function Ms = rescale_movie(M, orig_clim)
-% Scale movies to use a clim range of [0 1]. Typically used for making
-% side-by-side videos for demonstrations.
+function Ms = rescale_movie(M, orig_clim, varargin)
+% Scale movies to use a clim range of [0 1]. Other options to rescale the
+% movie in space. Typically used for making side-by-side videos for 
+% presentation purposes.
 
-Ms = zeros(size(M), 'single');
+new_size = [NaN NaN];
+for k = 1:length(varargin)
+    if ischar(varargin{k})
+        switch lower(varargin{k})
+            case 'height'
+                new_size(1) = varargin{k+1};
+            case 'width'
+                new_size(2) = varargin{k+1};
+        end
+    end
+end
 
-for k = 1:size(M,3)
+[h, w, num_frames] = size(M);
+if all(isnan(new_size)) % No resizing requested
+    new_size = [h, w];
+else
+    % Fill in missing dimension, if any
+    A = imresize(M(:,:,1), new_size);
+    new_size = size(A);   
+end
+
+Ms = zeros(new_size(1), new_size(2), num_frames, 'single');
+
+for k = 1:num_frames
     frame = M(:,:,k);
+    frame = imresize(frame, new_size);
     frame = (frame - orig_clim(1)) / diff(orig_clim);
     
     % Bound pixel values to [0 1]
