5dc3373b6e94bcea57d383ffaebe89fceb791dda
Tony <kimth@stanford.edu>
2018-04-11 19:04:35 -0700

    Critical error in laser_off and laser_on!

--- opto/compute_pvals.m
+++ opto/compute_pvals.m
@@ -9,7 +9,7 @@ for k = 1:num_cells
     events = ds.get_events_full(k);
     num_events(k) = size(events,1);
     
-    [p1, p2] = count_opto_events(events(:,2), laser_on, laser_off);
+    [p1, p2] = count_opto_events(events(:,2), laser_off, laser_on);
     pvals(k) = min([p1, p2]);
 end
 
