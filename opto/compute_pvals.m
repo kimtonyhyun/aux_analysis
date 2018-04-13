f70edb02e16dbe25727a26ab9bd9e002e878de53
Tony <kimth@stanford.edu>
2018-04-12 21:19:21 -0700

    Handle cases with no events returned

--- opto/compute_pvals.m
+++ opto/compute_pvals.m
@@ -9,8 +9,12 @@ for k = 1:num_cells
     events = ds.get_events_full(k);
     num_events(k) = size(events,1);
     
-    [p1, p2] = count_opto_events(events(:,2), laser_off, laser_on);
-    pvals(k) = min([p1, p2]);
+    if isempty(events)
+        pvals(k) = Inf;
+    else
+        [p1, p2] = count_opto_events(events(:,2), laser_off, laser_on);
+        pvals(k) = min([p1, p2]);
+    end
 end
 
 % Sort by p-value
