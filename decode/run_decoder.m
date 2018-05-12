92bf62197304a67a714ab3c39d1e9bda39f35a5e
kimth <kimth@stanford.edu>
2018-05-12 12:40:00 -0700

    Encapsulate position visualization

--- decode/run_decoder.m
+++ decode/run_decoder.m
@@ -9,16 +9,23 @@ ds = DaySummary(sources, 'cm01-fix', 'noprobe');
 
 %% Decoder params
 
-pos = 0.1;
+wn_trials = ds.filter_trials('start', 'west', 'end', 'north');
+ws_trials = ds.filter_trials('start', 'west', 'end', 'south');
+trials = wn_trials | ws_trials;
+
+pos = 0.25;
 
 alg = my_algs('linsvm', 0.1); % L1 reg
 num_runs = 512;
 
+%% Confirm position sampling
+pos_frames = compute_pos_frames(ds, pos);
+plot_centroids(ds, {wn_trials, ws_trials}, pos_frames);
+title(sprintf('%s: Sampled positions for pos=%.2f', dataset_name, pos));
+
 %% END decode
 
-trials = ds.filter_trials('start', 'west'); % Changing path
-
-[traces_perf, info] = decode_end(alg, ds, pos, trials, 'traces', num_runs);
+[traces_perf, decode_info] = decode_end(alg, ds, pos, trials, 'traces', num_runs);
 % copy_perf = decode_end(alg, ds, pos, trials, 'copy', num_runs);
 % copyzeroed_perf = decode_end(alg, ds, pos, trials, 'copy_zeroed', num_runs);
 % box_perf = decode_end(alg, ds, pos, trials, 'box', num_runs);
@@ -30,7 +37,7 @@ num_nonzero_weights = zeros(1, num_runs);
 eps = 1e-4;
 
 for k = 1:num_runs
-    b = info.models{k}.Beta;
+    b = decode_info.models{k}.Beta;
     bs(:,k) = b;
     
     max_b = max(abs(b));
@@ -62,12 +69,12 @@ title('Aligned to gate close');
 %%
 
 subplot(122);
-[raster, info] = ds.get_aligned_trace(cell_idx, trial_inds, info.pos_frames(trial_inds,1), 'apply_trial_offset');
+[raster, info] = ds.get_aligned_trace(cell_idx, trial_inds, decode_info.pos_frames(trial_inds,1), 'apply_trial_offset');
 imagesc(info.aligned_time, 1:num_trials, raster);
 title(sprintf('Aligned to pos=%.1f', pos));
 
 trial_inds = find(ds.filter_trials('start', 'west', 'end', 'north'));
-[raster2, info2] = ds.get_aligned_trace(cell_idx, trial_inds, info.pos_frames(trial_inds,1), 'apply_trial_offset');
+[raster2, info2] = ds.get_aligned_trace(cell_idx, trial_inds, decode_info.pos_frames(trial_inds,1), 'apply_trial_offset');
 % title('West --> South');
 % subplot(122);
 % ds.plot_cell_raster(cell_idx, 'start', 'west', 'end', 'north');
