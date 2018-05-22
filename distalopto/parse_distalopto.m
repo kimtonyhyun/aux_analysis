24224921d80e797e57ba384e8e8d3f4c2f649086
kimth <kimth@stanford.edu>
2018-05-22 15:09:05 -0700

    Parse lick and velocity

--- distalopto/parse_distalopto.m
+++ distalopto/parse_distalopto.m
@@ -1,14 +1,22 @@
-function trial_frame_indices = parse_distalopto(source)
+function [trial_frame_indices, velocity, frame_data] = parse_distalopto(source)
 % Parse distalopto behavioral data from its Saleae CSV log.
 % TODO: Handle dropped frames
 
 frame_data = parse_frames(source);
+num_trials = max(frame_data(:,1));
 
-num_trials = length(unique(frame_data(:,1)));
+% Convert position and velocity to cm/s
+cpr = 500; % clicks per rotation
+R = 5.5; % cm, approximate effective radius on InnoWheel
+frame_data(:,6) = 2*pi*R*frame_data(:,6)/cpr;
+frame_data(:,7) = 2*pi*R*frame_data(:,7)/cpr;
+
 trial_frame_indices = zeros(num_trials, 4);
+velocity = cell(num_trials,1);
 
 for trial_idx = 1:num_trials
     trial_frames = (frame_data(:,1)==trial_idx);
+    
     start_frame = find(trial_frames, 1, 'first');
     last_frame = find(trial_frames, 1, 'last');
     
@@ -31,7 +39,13 @@ for trial_idx = 1:num_trials
     
     trial_frame_indices(trial_idx,:) = ...
         [start_frame cs_start_frame us_start_frame last_frame];
+    
+    velocity{trial_idx} = frame_data(trial_frames, 7)';
 end
+
+% For viewing convenience, convert to table
+frame_data = array2table(frame_data,...
+    'VariableNames', {'Trial', 'AbsTime', 'CS', 'Lick', 'US', 'Position', 'Velocity'});
 
 end % parse_distalopto
 
@@ -41,15 +55,20 @@ function frame_data = parse_frames(source)
 %   -> frame_data(k,1): Trial index associated with the k-th frame
 %   -> frame_data(k,2): Absolute time at the beginning of the k-th frame
 %   -> frame_data(k,3): 1 if CS was active at the k-th frame, 0 otherwise
-%   -> frame_data(k,4): 1 if US was active at the k-th frame, 0 otherwise
-%   -> frame_data(k,5): Interpolated position (units of encoder "counts" 
+%   -> frame_data(k,4): Number of detected licks between the (k-1) and k-th
+%                       frames.
+%   -> frame_data(k,5): 1 if US was activated between the (k-1) and k-th
+%                       frames.
+%   -> frame_data(k,6): Interpolated position (units of encoder "counts" 
 %           at the beginning of the k-th frame
+%   -> frame_data(k,7): Interpolated velocity (counts/sec) at the k-th
+%           frame.
 %
 
 % Saleae channels
 encA_ch = 0;
 encB_ch = 1;
-
+lick_ch = 2;
 scope_en_ch = 3;
 cs_ch = 5;
 us_ch = 6;
@@ -70,7 +89,7 @@ fprintf('Parsing encoder data... '); tic;
 encA = data(:,2+encA_ch);
 encB = data(:,2+encB_ch);
 
-pos = zeros(num_rows, 2); % Preallocate output
+pos = zeros(num_rows, 2); % Preallocate output. Format: [time pos(click)]
 idx = 0;
 curr_pos = 0;
 for k = 2:num_rows
@@ -95,10 +114,11 @@ fprintf('Parsing frame data... '); tic;
 scope_en = data(:,2+scope_en_ch);
 frame_clock = data(:,2+frame_clock_ch);
 cs_trace = data(:,2+cs_ch);
+lick_trace = data(:,2+lick_ch);
 us_trace = data(:,2+us_ch);
 
-% Preallocate output. Format: [Trial-idx Time CS US]
-frame_data = zeros(num_rows,4);
+% Preallocate output. Format: [Trial-idx Time CS lick US]
+frame_data = zeros(num_rows, 5);
 frame_idx = 0;
 trial_idx = 0;
 
@@ -106,28 +126,56 @@ trial_idx = 0;
 % the duration of the US signal is shorter than the frame clock period --
 % hence we can easily miss the US pulse. Hence, we employ a flag.
 us_detected = 0;
+
+% Similarly, we employ a counter on the lickometer trace. Empirically, this
+% scheme doesn't seem to be explicitly necessary at 30 Hz recordings, as
+% the length of each individual lick contact appears to be longer than the
+% frame period. Nevertheless, we employ the counter so that the code
+% doesn't break in case of slower imaging rates.
+num_licks = 0;
+
+% Note that the use of the flag/counter for US/lick means that there will
+% be 1 sample lag. Namely, if a US occurs _during_ frame N (i.e. after the
+% frame clock went active) then the US will be marked as present on the
+% ensuing frame N+1.
+
 for k = 2:length(times)
-    if (~scope_en(k-1) && scope_en(k)) %% Rising edge on scope_enable
+    if (~scope_en(k-1) && scope_en(k)) % Rising edge on scope_enable
         trial_idx = trial_idx + 1;
+        % Don't accumulate licks across trials. TODO: Store the number of
+        % licks during ITI
+        num_licks = 0;
     end
     
-    if (~us_trace(k-1) && us_trace(k)) %% Rising edge on US trace
+    if (~us_trace(k-1) && us_trace(k)) % Rising edge on US trace
         us_detected = 1;
     end
     
-    if (~frame_clock(k-1) && frame_clock(k)) %% Rising edge on frame clock
+    if (~lick_trace(k-1) && lick_trace(k)) % Rising edge on lick trace
+        num_licks = num_licks + 1;
+    end
+    
+    if (~frame_clock(k-1) && frame_clock(k)) % Rising edge on frame clock
         frame_idx = frame_idx + 1;
-        frame_data(frame_idx,:) = [trial_idx times(k) cs_trace(k) us_detected];
+        frame_data(frame_idx,:) = [trial_idx times(k) cs_trace(k) num_licks us_detected];
+        num_licks = 0;
         us_detected = 0;
     end
 end
 
 frame_data = frame_data(1:frame_idx,:);
 
-% Interpolated position at start of each frame
+% Interpolate position and velocity at the frame clock
+%------------------------------------------------------------
 frame_times = frame_data(:,2);
 pos_interp = interp1(pos(:,1), pos(:,2), frame_times);
-frame_data = [frame_data pos_interp];
+
+dt = 0.1; % seconds, used for velocity estimation
+pos2 = interp1(pos(:,1), pos(:,2), frame_times+dt/2);
+pos1 = interp1(pos(:,1), pos(:,2), frame_times-dt/2);
+vel = (pos2-pos1)/dt;
+
+frame_data = [frame_data pos_interp vel];
 
 t = toc; fprintf('Done in %.1f seconds!\n', t);
 end % parse_frames
