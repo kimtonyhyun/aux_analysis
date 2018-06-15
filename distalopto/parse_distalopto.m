f797d2f4109256536dcd2865cf46e4434f7f936f
Tony <kimth@stanford.edu>
2018-06-15 10:21:00 -0700

    Parse ITI data

--- distalopto/parse_distalopto.m
+++ distalopto/parse_distalopto.m
@@ -1,19 +1,19 @@
 function [trial_frame_indices, frame_data] = parse_distalopto(source)
 % Parse distalopto behavioral data from its Saleae CSV log. Generates a
 % PlusMaze-like text file with trial frame boundaries, and a MAT file
-% (behavior.mat) containing running velocity, licking, and trial
-% correctness.
+% (behavior.mat) containing running velocity, licking, trial correctness,
+% and ITI information.
 %
 % TODO: Dropped frame correction!
 
-frame_data = parse_frames(source);
-num_trials = max(frame_data(:,1));
+[frame_data, iti, num_trials] = parse_frames(source);
 
 % Convert position and velocity to cm/s
 cpr = 500; % clicks per rotation
 R = 5.5; % cm, approximate effective radius on InnoWheel
-frame_data(:,6) = 2*pi*R*frame_data(:,6)/cpr;
-frame_data(:,7) = 2*pi*R*frame_data(:,7)/cpr;
+frame_data(:,6) = 2*pi*R/cpr*frame_data(:,6);
+frame_data(:,7) = 2*pi*R/cpr*frame_data(:,7);
+iti(:,3) = 2*pi*R/cpr*iti(:,3); %#ok<*NASGU>
 
 trial_frame_indices = zeros(num_trials, 4);
 
@@ -58,7 +58,7 @@ end
 write_distalopto(trial_frame_indices, rewarded);
 
 % Write behavioral metadata to MAT file
-save('behavior.mat', 'velocity', 'rewarded', 'licks');
+save('behavior.mat', 'velocity', 'rewarded', 'licks', 'iti');
 
 % For viewing convenience, convert to table
 frame_data = array2table(frame_data,...
@@ -66,7 +66,7 @@ frame_data = array2table(frame_data,...
 
 end % parse_distalopto
 
-function frame_data = parse_frames(source)
+function [frame_data, iti_data, num_trials] = parse_frames(source)
 % Output:
 % - frame_data: [num_frames x 5] table where,
 %   -> frame_data(k,1): Trial index associated with the k-th frame
@@ -134,6 +134,9 @@ cs_trace = data(:,2+cs_ch);
 lick_trace = data(:,2+lick_ch);
 us_trace = data(:,2+us_ch);
 
+% FIRST PASS through the data for TRIAL data
+%------------------------------------------------------------
+
 % Preallocate output. Format: [Trial-idx Time CS lick US]
 frame_data = zeros(num_rows, 5);
 frame_idx = 0;
@@ -183,7 +186,6 @@ end
 frame_data = frame_data(1:frame_idx,:);
 
 % Interpolate position and velocity at the frame clock
-%------------------------------------------------------------
 frame_times = frame_data(:,2);
 pos_interp = interp1(pos(:,1), pos(:,2), frame_times);
 
@@ -193,6 +195,44 @@ pos1 = interp1(pos(:,1), pos(:,2), frame_times-dt/2);
 vel = (pos2-pos1)/dt;
 
 frame_data = [frame_data pos_interp vel];
+num_trials = max(frame_data(:,1));
+
+% SECOND PASS through data for ITI data
+%------------------------------------------------------------
+
+% ITI data. Format: [Duration (s), Num licks, Distance traveled (clicks)].
+%   iti_data(k,:) refers to the ITI _preceding_ the k-th trial. Note that
+%   in this scheme, the first trial may have an unusual ITI duration.
+iti_data = zeros(num_trials, 3);
+iti_times = zeros(num_trials, 2); % [Start, End]
+
+trial_idx = 1;
+num_licks = 0;
+for k = 2:length(times)
+    if ~scope_en(k) % Not in trial, hence ITI
+        if (~lick_trace(k-1) && lick_trace(k)) % Rising edge on lick trace
+            num_licks = num_licks + 1;
+        end
+    end
+    
+    if (~scope_en(k-1) && scope_en(k)) % Rising edge on trial. End ITI
+        iti_times(trial_idx,2) = times(k);
+        
+        iti_data(trial_idx,1) = diff(iti_times(trial_idx,:));
+        iti_data(trial_idx,2) = num_licks;
+        num_licks = 0;
+    end
+    
+    if (scope_en(k-1) && ~scope_en(k)) % Falling edge on trial. Start ITI
+        trial_idx = trial_idx + 1;
+        iti_times(trial_idx,1) = times(k);
+    end
+end
+
+for k = 1:num_trials
+    iti_pos = interp1(pos(:,1), pos(:,2), iti_times(k,:), 'linear', 'extrap');
+    iti_data(k,3) = diff(iti_pos);
+end
 
 t = toc; fprintf('Done in %.1f seconds!\n', t);
 end % parse_frames
