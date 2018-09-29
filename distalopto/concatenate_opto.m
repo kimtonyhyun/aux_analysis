963b70605678c07e578502b975886e335ade0582
kimth <kimth@stanford.edu>
2018-09-28 17:35:52 -0700

    Handle first and last frames

--- distalopto/concatenate_opto.m
+++ distalopto/concatenate_opto.m
@@ -12,6 +12,13 @@ for k = 1:num_trials
     fprintf('%s: Loading "%s"...\n', datestr(now), filename);
     M_trial = load_scanimage_tif(filename);
     
+    num_frames_in_trial = size(M_trial, 3);
+    
+    % It appears that the first and last frames in each trial are often
+    % dark (maybe the IR is being cut out?)
+    M_trial(:,:,1) = M_trial(:,:,2);
+    M_trial(:,:,num_frames_in_trial) = M_trial(:,:,num_frames_in_trial-1);
+    
     start_idx = num_frames_saved + 1;
     end_idx = start_idx + size(M_trial,3) - 1;
     M(:,:,start_idx:end_idx) = M_trial;
@@ -19,7 +26,7 @@ for k = 1:num_trials
     num_frames_saved = end_idx;
 end
 M = M(:,:,1:num_frames_saved);
-
+fprintf('%s: Done!\n', datestr(now));
 
 %% Inspect fluorescence stats
 F = compute_fluorescence_stats(M);
