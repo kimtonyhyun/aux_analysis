40f91d0cea9387f14f89144a14ffccda50265ec4
kimth <kimth@stanford.edu>
2018-12-23 11:50:55 -0800

    Back to legacy

--- opto/dealternate_opto.m
+++ opto/dealternate_opto.m
@@ -5,16 +5,16 @@ function M = dealternate_opto(M)
 num_frames = size(M,3);
 
 % LEGACY: 'optocontroller_20181217.bit' blanks the EVEN frames
-% if mod(num_frames,2) % Odd
-%     M(:,:,2:2:end) = M(:,:,1:2:end-1);
-% else
-%     M(:,:,2:2:end) = M(:,:,1:2:end);
-% end
+if mod(num_frames,2) % Odd
+    M(:,:,2:2:end) = M(:,:,1:2:end-1);
+else
+    M(:,:,2:2:end) = M(:,:,1:2:end);
+end
 
 % 'optocontroller_20181221.bit' blanks the ODD frames
-if mod(num_frames,2) % Total number of frames is odd
-    M(:,:,1:2:end-1) = M(:,:,2:2:end);
-    M(:,:,end) = M(:,:,end-1);
-else
-    M(:,:,1:2:end) = M(:,:,2:2:end);
-end
+% if mod(num_frames,2) % Total number of frames is odd
+%     M(:,:,1:2:end-1) = M(:,:,2:2:end);
+%     M(:,:,end) = M(:,:,end-1);
+% else
+%     M(:,:,1:2:end) = M(:,:,2:2:end);
+% end
