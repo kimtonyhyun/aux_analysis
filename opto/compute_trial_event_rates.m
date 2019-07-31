a8f234064d85248aa448a8457cfc236884ebe5de
kimth <kimth@stanford.edu>
2019-07-30 17:09:47 -0700

    reorg

--- opto/compute_trial_event_rates.m
+++ opto/compute_trial_event_rates.m
@@ -4,15 +4,15 @@ function event_rate_per_trial = compute_trial_event_rates(ds, cell_idx, event_ty
 event_rate_per_trial = zeros(ds.num_trials, 1);
 for k = 1:ds.num_trials
     eventdata = ds.trials(k).events{cell_idx};
-    switch event_type
-        case 'num_events'
-            e = size(eventdata,1);
-        case 'event_amp_sum'
-            if ~isempty(eventdata)
-                e = sum(eventdata(:,3));
-            else
-                e = 0;
-            end
+    if isempty(eventdata)
+        e = 0;
+    else
+        switch event_type
+            case 'num_events'
+                e = size(eventdata,1);
+            case 'event_amp_sum'
+                e = sum(eventdata(:,3));   
+        end
     end
     event_rate_per_trial(k) = e / ds.trials(k).time;
 end
