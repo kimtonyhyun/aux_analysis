c062e0f2eafed8631bc4860e80bf0c93bfda5606
kimth <kimth@stanford.edu>
2019-07-30 17:19:25 -0700

    strrep

--- opto/compute_pvals3.m
+++ opto/compute_pvals3.m
@@ -10,13 +10,17 @@ shuffle_savename = sprintf('shuffle_%s.mat', datestr(now, 'yymmdd-HHMMSS'));
 save(shuffle_savename, 'info', 'stats_sig');
 
 %% Visualize shuffle stats
+figure;
 visualize_shuffle(info);
 
 %% Inhibited traces
+figure;
 visualize_opto_traces(ds, info, 'inhibited');
 
 %% Disinhibited traces
+figure;
 visualize_opto_traces(ds, info, 'disinhibited');
 
 %% Show as cell map
+figure;
 visualize_opto_cellmap(ds, info);
