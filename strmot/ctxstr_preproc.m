ec31cf2b4d8da6a3b82cb99cb3baf023e2a2d5e3
kimth <kimth@stanford.edu>
2020-01-10 17:12:15 -0800

    Both ctx and str

--- strmot/ctxstr_preproc.m
+++ strmot/ctxstr_preproc.m
@@ -4,35 +4,68 @@ dataset_name = dirname;
 fprintf('%s: Preprocessing "%s"...\n', datestr(now), dataset_name);
 
 ctx_stem = sprintf('%s-ctx', dataset_name);
+str_stem = sprintf('%s-str', dataset_name);
 
-%% Ctx: Convert to HDF5
+%% Convert to HDF5
 
-source = 'ctx_00001.tif';
-Mg = load_scanimage_tif2(source, 'odd'); % Green channel
+% ctx
+% ctx_source = 'ctx_00001.tif';
+% 
+% Mg = load_scanimage_tif2(ctx_source, 'odd'); % Green channel
+% Fg = compute_fluorescence_stats(Mg);
+% Mg = Mg - int16(mean(Fg(:,1)));
+% save_movie_to_hdf5(Mg, [ctx_stem '.hdf5']);
+% clear Mg Fg;
+% 
+% Mr = load_scanimage_tif2(ctx_source, 'even'); % Red channel
+% Fr = compute_fluorescence_stats(Mr);
+% Mr = Mr - int16(mean(Fr(:,1)));
+% save_movie_to_hdf5(Mr, [ctx_stem '-tdt.hdf5']);
+% clear Mr Fr;
+
+% str
+str_source = 'str_00001.tif';
+
+Mg = load_scanimage_tif2(str_source, 'odd'); % Green channel
 Fg = compute_fluorescence_stats(Mg);
 Mg = Mg - int16(mean(Fg(:,1)));
-save_movie_to_hdf5(Mg, [ctx_stem '.hdf5']);
+save_movie_to_hdf5(Mg, [str_stem '.hdf5']);
 clear Mg Fg;
 
-Mr = load_scanimage_tif2(source, 'even'); % Red channel
+Mr = load_scanimage_tif2(str_source, 'even'); % Red channel
 Fr = compute_fluorescence_stats(Mr);
 Mr = Mr - int16(mean(Fr(:,1)));
-save_movie_to_hdf5(Mr, [ctx_stem '-tdt.hdf5']);
+save_movie_to_hdf5(Mr, [str_stem '-tdt.hdf5']);
 clear Mr Fr;
 
 fprintf('%s: DONE with HDF5 conversion!\n', datestr(now));
 
-%% Ctx: Meancorr
+%% Meancorr
 
-meancorr_movie([ctx_stem '.hdf5'], '');
-meancorr_movie([ctx_stem '-tdt.hdf5'], '');
+% ctx
+% meancorr_movie([ctx_stem '.hdf5'], '');
+% meancorr_movie([ctx_stem '-tdt.hdf5'], '');
+
+% str
+meancorr_movie([str_stem '.hdf5'], '');
+meancorr_movie([str_stem '-tdt.hdf5'], '');
 
 fprintf('%s: DONE with meancorr_movie!\n', datestr(now));
 
-%% Ctx: Normcorre
+%% Normcorre
 
-run_normcorre([ctx_stem '-tdt_uc.hdf5'], '');
-load([ctx_stem '-tdt_uc_nc.mat']);
-apply_shifts([ctx_stem '_uc.hdf5'], shifts, info.nc_options);
+% ctx
+% run_normcorre([ctx_stem '-tdt_uc.hdf5'], '');
+% load([ctx_stem '-tdt_uc_nc.mat']);
+% apply_shifts([ctx_stem '_uc.hdf5'], shifts, info.nc_options);
+
+% str
+run_normcorre([str_stem '-tdt_uc.hdf5'], '');
+A = compute_mean_image([str_stem '-tdt_uc_nc.hdf5']);
+save([str_stem '-tdt.mat'], A);
+
+load([str_stem '-tdt_uc_nc.mat']);
+apply_shifts([str_stem '_uc.hdf5'], shifts, info.nc_options);
+bin_movie_in_time([str_stem '_uc_nc.hdf5'], '', 12);
 
 fprintf('%s: DONE with run_normcorre!\n', datestr(now));
