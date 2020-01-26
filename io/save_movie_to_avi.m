59b99dacf9cd1399fc90d06cdaca3e372803aa0c
kimth <kimth@stanford.edu>
2020-01-26 10:38:49 -0800

    Slight cleanup of avi export

--- io/save_movie_to_avi.m
+++ io/save_movie_to_avi.m
@@ -1,14 +1,22 @@
-function save_movie_to_avi(M, frame_rate)
+function save_movie_to_avi(M, scale)
+% Save movie matrix into an uncompressed AVI file for presentation
+% purposes. Consider compressing the output AVI file into MP4 using
+% external software such as Handbrake.
 
-switch class(M)
-    case {'uint16'}
-        scale = [0 0.9*max(M(:))];
-    case {'single'}
-%         scale = compute_movie_scale(M);
-        scale = [-0.02 0.05];
+% Default parameters:
+frame_rate = 30; % Hz. Going below 30 Hz usually doesn't look good.
+output_name = 'out.avi';
+
+if ~exist('scale', 'var') % Scaling not provided
+    switch class(M)
+        case {'uint16'}
+            scale = [0 0.9*max(M(:))];
+        case {'single'}
+            scale = compute_movie_scale(M);
+    end
 end
 
-writerObj = VideoWriter('out.avi', 'Uncompressed AVI');
+writerObj = VideoWriter(output_name, 'Uncompressed AVI');
 writerObj.FrameRate = frame_rate;
 open(writerObj);
 for i = 1:size(M,3)
