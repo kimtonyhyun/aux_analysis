585c647a4242f9a08f0b5abd32dcbbab69f16248
kimth <kimth@stanford.edu>
2020-02-16 16:23:29 -0800

    Tweak script

--- strmot/make_ctxstr_sbs.m
+++ strmot/make_ctxstr_sbs.m
@@ -1,15 +1,20 @@
 clear all; close all;
 
+% Source filenames
+str_source = 'oh06-0206-str_uc_nc.hdf5';
+ctx_source = 'oh06-0206-ctx_uc_nc.hdf5';
+beh_source = 'test.mp4';
+
 % Output (side-by-side) video parameters
 fps = 30;
-sbs_duration = 20; % s
+sbs_duration = 20; % sec
 
 ctx_bin = 8;
 str_bin = 45/30*ctx_bin; % Ratio of acquisition frame rates
 
 num_output_frames = fps * sbs_duration;
 
-%%
+%% Timing
 
 data = load('ctxstr.mat'); % Output of 'parse_ctxstr.m'
 
@@ -20,9 +25,7 @@ beh_skip = sum(data.behavior.frame_times < data.str.frame_times(1));
 beh_frames = find_nearest_frames(data.str.frame_times, 1:str_bin:str_bin*num_output_frames,...
     data.behavior.frame_times);
 
-%%
-
-str_source = 'oh5-1104-str_uc_nc.hdf5';
+%% Extract Str sub-movie
 
 num_str_frames_to_load = num_output_frames * str_bin;
 M_str = load_movie_from_hdf5(str_source, [1 num_str_frames_to_load]);
@@ -31,9 +34,7 @@ clear M_str;
 bin_movie_in_time('str_chunk.hdf5', 'sbs-str.hdf5', str_bin);
 delete('str_chunk.hdf5');
 
-%%
-
-ctx_source = 'oh5-1104-ctx_uc_nc.hdf5';
+%% Extract Ctx sub-movie
 
 num_ctx_frames_to_load = num_output_frames * ctx_bin;
 M_ctx = load_movie_from_hdf5(ctx_source, [ctx_skip+1 ctx_skip+num_ctx_frames_to_load]);
@@ -42,9 +43,8 @@ clear M_ctx;
 bin_movie_in_time('ctx_chunk.hdf5', 'sbs-ctx.hdf5', ctx_bin);
 delete('ctx_chunk.hdf5');
 
-%%
+%% Extract Behavior sub-movie
 
-beh_source = 'test.mp4';
 vid = VideoReader(beh_source);
 
 M = zeros(1080, 810, num_output_frames, 'uint8');
@@ -70,15 +70,17 @@ save_movie_to_hdf5(M, 'sbs-beh.hdf5');
 clear M;
 
 %%
+clear all;
 
 M_ctx = load_movie('sbs-ctx.hdf5');
 M_str = load_movie('sbs-str.hdf5');
 M_beh = load_movie('sbs-beh.hdf5');
-M_beh = M_beh(1:1024,:,:);
+% M_beh = M_beh(1:1024,:,:); % Keep top
+M_beh = M_beh(end-1023:end,:,:); % Keep bottom
 
 %%
 
-ctx_clim = [0 5];
+ctx_clim = [0 6];
 M_ctx2 = uint8(255*(M_ctx-ctx_clim(1))/(ctx_clim(2)-ctx_clim(1)));
 
 str_clim = [0 5];
