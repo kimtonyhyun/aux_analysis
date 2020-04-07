0ad90aca7d0f75ac183000df17ed376540ede6c7
kimth <kimth@stanford.edu>
2020-04-07 09:40:08 -0700

    Save both directions

--- prefrontal/prefrontal_match_all.m
+++ prefrontal/prefrontal_match_all.m
@@ -1,30 +1,36 @@
 clear;
 
-mouse_name = 'c11m1';
+mouse_name = 'c11m3';
 get_id = @(x) x(end-1:end);
 
 [ds_list, num_datasets] = load_all_ds(mouse_name, 'cm/clean', get_id);
 
 %% Select primary day, and match all others to it
 
-primary_day = 6;
+primary_day = 1;
 fprintf('%s: Selected "%s" as primary day\n', datestr(now), ds_list{primary_day,1});
-other_days = setdiff(1:num_datasets, primary_day);
 
 ds1 = ds_list{primary_day,2};
-k = 1;
+k = primary_day + 1;
 
 %% Run alignment
 
 close all;
 
-other_day = other_days(k);
-ds2 = ds_list{other_day,2};
+ds2 = ds_list{k,2};
 [m_1to2, m_2to1, info] = run_alignment(ds1, ds2, 'num_points', 4);
 
 %% Happy with alignment? Save and move on
 
-savename = sprintf('match_%s_%s.mat', ds_list{primary_day,1}, ds_list{other_day,1});
+savename = sprintf('match_%s_%s.mat', ds_list{primary_day,1}, ds_list{k,1});
 save(savename, 'm_1to2', 'm_2to1', 'info');
 fprintf('%s: Saved "%s"\n', datestr(now), savename);
+
+% Also save the "reverse" match file
+[m_1to2, m_2to1, info] = reverse_alignment_outputs(m_1to2, m_2to1, info);
+
+savename = sprintf('match_%s_%s.mat', ds_list{k,1}, ds_list{primary_day,1});
+save(savename, 'm_1to2', 'm_2to1', 'info');
+fprintf('%s: Saved "%s"\n', datestr(now), savename);
+
 k = k + 1;
