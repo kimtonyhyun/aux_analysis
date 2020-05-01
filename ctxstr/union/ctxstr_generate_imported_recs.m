e3bd3a4d95c4784cafeab319dae6015f8c08911e
kimth <kimth@stanford.edu>
2020-04-30 22:43:58 -0700

    Simplify ctxstr union procedure

---
+++ ctxstr/union/ctxstr_generate_imported_recs.m
@@ -0,0 +1,60 @@
+clear;
+
+dff_movie = dir('*.hdf5');
+dff_movie = dff_movie.name;
+fprintf('%s: Loading "%s"...\n', datestr(now), dff_movie);
+M = load_movie(dff_movie);
+
+%% Generate Rec files from the match results
+
+get_id = @(x) x(end-3:end);
+
+matches = dir('match_*.mat');
+num_matches = length(matches);
+
+for k = 1:num_matches
+    match_name = matches(k).name;
+    match_data = load(matches(k).name);
+    
+    [~, match_name] = fileparts(match_name); % Skip the extension
+    match_date = get_id(match_name);
+    
+    [~, rec_savename] = get_dff_traces(match_data.info.filters_2to1.im, M);
+    
+    rec_dirname = fullfile('imports', sprintf('from_%s', match_date));
+    mkdir(rec_dirname);
+    movefile(rec_savename, rec_dirname);
+end
+
+%% Resolve the incoming filters prior to sorting
+
+[imports, num_imports] = load_all_ds('imports/from_');
+
+ds = DaySummary([], 'cnmf1/iter2'); % Original rec. FIXME: Hard-coded
+num_orig_cells = ds.num_classified_cells;
+md = create_merge_md([{ds}; imports(:,2)]);
+
+%%
+
+res_list = resolve_merged_recs(md, M,...
+                'norm_traces',...
+                'names', [{'Original'}; imports(:,1)]);
+save_resolved_recs(res_list, md);
+
+% Move the output rec into the following subdirectory: 'union/resolved'
+
+%%
+
+% Final steps:
+%   1) Copy labels from original rec to the resolved rec
+%   2) Manually classify remaining cells (i.e. newly imported cells)
+
+clearvars -except num_orig_cells M;
+
+dsr = DaySummary('', 'union/resolved');
+
+% The first cells in the resolved DS are taken directly from the original
+% DaySummary. Sort the rest!
+dsr.set_labels(1:num_orig_cells);
+
+classify_cells(dsr, M);
--- ctxstr/union/ctxstr_match_all.m
+++ ctxstr/union/ctxstr_match_all.m
@@ -1,9 +1,11 @@
 clear;
 
-mouse_name = 'c11m2';
-get_id = @(x) x(end-1:end);
+mouse_name = 'oh05';
+region = 'ctx'; % Either 'ctx' or 'str'
+get_id = @(x) x(end-3:end);
 
-[ds_list, num_datasets] = load_all_ds(mouse_name, 'cm/clean', get_id);
+[ds_list, num_datasets] = load_all_ds(mouse_name,...
+    fullfile(region, 'cnmf1/iter2'), get_id);
 
 %% Select primary day, and match all others to it
 
@@ -22,14 +24,14 @@ ds2 = ds_list{k,2};
 
 %% Happy with alignment? Save and move on
 
-savename = sprintf('match_%s_%s.mat', ds_list{primary_day,1}, ds_list{k,1});
+savename = sprintf('match_%s_%s_%s.mat', region, ds_list{primary_day,1}, ds_list{k,1});
 save(savename, 'm_1to2', 'm_2to1', 'info');
 fprintf('%s: Saved "%s"\n', datestr(now), savename);
 
 % Also save the "reverse" match file
 [m_1to2, m_2to1, info] = reverse_alignment_outputs(m_1to2, m_2to1, info);
 
-savename = sprintf('match_%s_%s.mat', ds_list{k,1}, ds_list{primary_day,1});
+savename = sprintf('match_%s_%s_%s.mat', region, ds_list{k,1}, ds_list{primary_day,1});
 save(savename, 'm_1to2', 'm_2to1', 'info');
 fprintf('%s: Saved "%s"\n', datestr(now), savename);
 
