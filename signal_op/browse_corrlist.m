89fe845892160971a5ee1a16ed42a1733cb5b5b8
kimth <kimth@stanford.edu>
2020-07-14 21:09:32 -0700

    Make general

--- signal_op/browse_corrlist.m
+++ signal_op/browse_corrlist.m
@@ -1,6 +1,6 @@
-function show_ctxstr_corrs(corrlist, ds1, ds2)
+function browse_corrlist(corrlist, ds1, ds2)
 
-y_offset = 0.2;
+y_offset = 0.0;
 y_range = [-0.1 1.1+y_offset];
 
 % Set up figure
@@ -18,7 +18,10 @@ xlim([1 length(tr_i)]);
 ylim(y_range);
 set(gca, 'TickLength', [0 0]);
 
-subplot(3,1,[2 3]);
+% h_cellmap1 = subplot(3,3,[4 7]);
+% h_cellmap2 = subplot(3,3,[5 8]);
+
+subplot(3,3,[6 9]);
 h_corr = plot(tr_i, tr_i, '.k');
 xlim([-0.1 1.1]);
 ylim([-0.1 1.1]);
@@ -37,10 +40,18 @@ for k = 1:size(corrlist, 1)
     h_tr1.YData = tr_i + y_offset; 
     h_tr2.YData = tr_j;
     xlim([1 length(tr_i)]);
+    ylim(y_range);
     title(sprintf('Corr=%.4f', c));
     
     h_corr.XData = tr_i;
     h_corr.YData = tr_j;
+
+%     % Show cell maps
+%     subplot(h_cellmap1);
+%     ds1.highlight_cell(i);
+%     subplot(h_cellmap2);
+%     ds2.highlight_cell(j);
+%     zoom on; % Undo 'datacursormode' within 'highlight_cell'
     
     drawnow;
     pause;
