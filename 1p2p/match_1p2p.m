285ee1872196247fe060d6361b144d3c10ae4c3f
kimth <kimth@stanford.edu>
2020-10-09 12:32:13 -0700

    Allow continuation

--- 1p2p/match_1p2p.m
+++ 1p2p/match_1p2p.m
@@ -1,7 +1,28 @@
-function matched_corrlist = match_1p2p(ds1, ds2, tform)
+function matched_corrlist = match_1p2p(ds1, ds2, tform, varargin)
 % For each 2P cell in ds2, attempt to find a matching 1P cell in ds1.
 
 app_name = '1P/2P matching';
+
+num_cells1 = ds1.num_classified_cells;
+num_cells2 = ds2.num_classified_cells;
+
+matched_corrlist = zeros(num_cells2, 3); % Preallocate output
+
+for k = 1:length(varargin)
+    if ischar(varargin{k})
+        switch lower(varargin{k})
+            case 'matched_corrlist'
+                % Continue from previously computed result
+                matched_corrlist2 = varargin{k+1};
+                for m = 1:size(matched_corrlist2,1)
+                    j = matched_corrlist2(m,2);
+                    if j ~= 0
+                        matched_corrlist(j,:) = matched_corrlist2(m,:);
+                    end
+                end
+        end
+    end
+end
 
 % First, pre-compute all correlations. Results are sorted by correlation
 % value (descending).
@@ -10,16 +31,13 @@ corrlist = compute_corrlist(ds1, ds2);
 % Sort corrlist by ds_source cell index
 corrlist = sortrows(corrlist, 2, 'ascend');
 
-num_cells1 = ds1.num_classified_cells;
-num_cells2 = ds2.num_classified_cells;
-
-% Preallocate output
-matched_corrlist = zeros(num_cells2, 3);
-
 hf = figure;
 
+j = find(matched_corrlist(:,2)>0, 1, 'last'); % Loops over ds2 cells
+if isempty(j)
+    j = 1;
+end
 i = 1; % Loops over ds1 cells
-j = 1; % Loops over ds2 cells
 while (1)
     % Block of corrlist for the j-th cell in ds2
     rows = (1+(j-1)*num_cells1):(j*num_cells1);
@@ -53,8 +71,8 @@ while (1)
             switch resp(1)
                 case {'c', 'm', 'y'} % Indicate match
                     matched_corrlist(j,:) = corrdata;
-                    fprintf('  1P cell=%d assigned to 2P cell=%d!\n',...
-                        corrdata(1), corrdata(2));
+                    fprintf('  2P cell=%d matched to 1P cell=%d!\n',...
+                        corrdata(2), corrdata(1));
 
                     % Increment 2P cell index
                     j = j + 1;
