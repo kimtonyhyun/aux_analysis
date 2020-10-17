ce15bcef65e844307445a561fc0c7ff847f18ef9
kimth <kimth@stanford.edu>
2020-10-17 13:23:42 -0700

    Detect motion onset

--- ctxstr/parse_ctxstr.m
+++ ctxstr/parse_ctxstr.m
@@ -1,5 +1,4 @@
 function [ctx, str, behavior, info] = parse_ctxstr(source)
-% Todo: Handle forced running signal
 
 % Define Saleae channels
 encA_ch = 0;
@@ -20,45 +19,80 @@ t = toc; fprintf('Done in %.1f seconds!\n', t);
 % Parse behavioral data at full resolution
 %------------------------------------------------------------
 % Encoder
-pos = parse_encoder(data, encA_ch, encB_ch); % [time enc_count]
 cpr = 500; % clicks per rotation
-R = 5.5; % cm, approximate effective radius on InnoWheel
-pos(:,2) = 2*pi*R/cpr*pos(:,2); % Convert to cm
-fprintf('Encoder: %.1f cm traveled over %.1f seconds (%.1f minutes)\n',...
-    pos(end,2), pos(end,1), pos(end,1)/60);
+pos = parse_encoder(data, encA_ch, encB_ch); % [time enc_count]
+fprintf('Encoder: %.1f rotations over %.1f seconds (%.1f minutes)\n',...
+    pos(end,2)/cpr, pos(end,1), pos(end,1)/60);
 
-% Compute velocity
+% Velocity
+R = 5.5; % cm, approximate effective radius on InnoWheel FIXME
+pos_cm = 2*pi*R/cpr*pos(:,2); % Convert to distance (cm)
+
 dt = 0.25; % seconds, used for velocity estimation
 t = dt:dt:(times(end)-dt);
-pos2 = interp1(pos(:,1), pos(:,2), t+dt/2);
-pos1 = interp1(pos(:,1), pos(:,2), t-dt/2);
+pos2 = interp1(pos(:,1), pos_cm, t+dt/2);
+pos1 = interp1(pos(:,1), pos_cm, t-dt/2);
 velocity = (pos2-pos1)/dt; % cm/s
 fprintf('Computed velocity over dt=%.3f second windows\n', dt);
 
 % Rewards
-us_times = find_pulses(data, us_ch);
+us_times = find_edges(data, us_ch);
+
 % Determine the number of pulses per reward. We assume pulses that occur in
 % a rapid (sub-second) succession are part of a single reward
-num_pulses_per_reward = sum((us_times(:,1) - us_times(1,1)) < 0.1);
+num_pulses_per_reward = sum((us_times - us_times(1)) < 0.1);
 fprintf('Detected %d pulses per reward\n', num_pulses_per_reward);
-% Each US event consists of two pulses. So skip every other pulse
-us_times = us_times(1:num_pulses_per_reward:end,1);
-% First reward is an automatic one at the beginning of session
+us_times = us_times(1:num_pulses_per_reward:end);
+
+% Movement onset
+% Onset defined by distance traveled towards reward threshold
+
+% First, determine the reward threshold from the position data. To do this,
+% split the position trace across individual trials, defined by the US.
+num_rewards = length(us_times);
+trial_inds = zeros(num_rewards,1);
+for k = 1:num_rewards
+    us_time = us_times(k);
+    trial_inds(k) = find(pos(:,1) > us_time, 1, 'first');
+end
+
+% The first US is a freebie at the beginning of each session, unrelated to
+% animal movement.
 us_times = us_times(2:end);
-num_rewards = size(us_times,1);
-fprintf('Detected %d rewards, omitting the first (free) reward\n', num_rewards);
+num_rewards = num_rewards-1;
+fprintf('Detected %d rewards, excluding the first (free) reward\n', num_rewards);
+
+pos_by_trial = cell(num_rewards, 1);
+for k = 1:num_rewards
+    inds_k = trial_inds(k):trial_inds(k+1)-1;
+    pos_k = pos(inds_k,:);
+    pos_k(:,2) = pos_k(:,2) - pos_k(1,2); % "Reset" position after each US
+    pos_by_trial{k} = pos_k;
+end
+
+% Next, determine the empirical distance threhsold for reward
+us_threshold = mean(cellfun(@(x) x(end,2), pos_by_trial));
+fprintf('On average, reward delivered after %.1f encoder clicks\n', us_threshold);
+
+% Finally, determine the movement onset based on distance traveled
+movement_onset_threshold = 0.2;
+movement_onset_times = zeros(num_rewards,1);
+for k = 1:num_rewards
+    pos_k = pos_by_trial{k};
+    ind = find(pos_k(:,2) > movement_onset_threshold * us_threshold, 1, 'first');
+    movement_onset_times(k) = pos_k(ind,1);
+end
 
 % Licks:
 % Note: Can filter here for lick durations
-lick_times = find_pulses(data, lick_ch); % [Rise_time Fall_time]
-lick_times = lick_times(:,1);
+lick_times = find_edges(data, lick_ch);
 
 % Lick response: Is there a detected lick within 1 s of water reward?
 lick_response_window = 1;
 lick_responses = zeros(num_rewards, 1);
 for k = 1:num_rewards
     us_time = us_times(k);
-    ind = find(lick_times > us_time, 1, 'first');
+    ind = find(lick_times >= us_time, 1, 'first');
     if ~isempty(ind)
         lick_time = lick_times(ind); % Timing of first lick after k-th US
         if (lick_time - us_time < lick_response_window)
@@ -77,8 +111,11 @@ fprintf('Found %d behavior frames at %.2f FPS\n',...
 
 % Package for output
 behavior.frame_times = behavior_frame_times;
-behavior.position = pos;
+behavior.position.cont = pos; % [time encoder-counts]
+behavior.position.by_trial = pos_by_trial; % Can be converted into a regular matrix by: pos_wrapped = cat(1, behavior.position.by_trial{:});
+behavior.position.us_threshold = us_threshold;
 behavior.velocity = [t' velocity']; % [time cm/s]
+behavior.movement_onset_times = movement_onset_times;
 behavior.us_times = us_times;
 behavior.lick_times = lick_times;
 behavior.lick_responses = lick_responses;
@@ -145,19 +182,26 @@ if (num_ctx_frames > 0) && (num_str_frames > 0)
 end
 
 % Save results to file
+%   info.hw_params: Hardware parameters
+%   info.saleae: Related to Saleae export
+%   info.params: Parameters used to compute intermediate quantities
+%                (e.g. velocity, reward response)
 %------------------------------------------------------------
-info.cpr = cpr; % Encoder counts per rotation
-info.R = R; % radius of wheel (cm)
-info.saleae_time_window = times([1 end]); % seconds
-info.dt = dt; % Used for velocity computation
-info.lick_response_window = lick_response_window; % seconds
-info.recording_length = times(end);
+info.hw_params.cpr = cpr; % Encoder counts per rotation
+info.hw_params.R = R; % radius of wheel (cm)
+info.saleae.time_window = times([1 end]); % seconds
+info.params.velocity_dt = dt; % Used for velocity computation
+info.params.movement_onset_threshold = movement_onset_threshold;
+info.params.lick_response_window = lick_response_window; % seconds
 
 save('ctxstr.mat', 'ctx', 'str', 'behavior', 'info');
 
 end % parse_ctxstr
 
 function generate_pmtext(outname, reward_frames, responses, imaging_fps, max_frames)
+    % Trial frames: [Trial-start Motion-onset US Trial-end]
+    %   Trial-start: 5 s prior to US
+    %   Motion-onset
     frame_offsets = imaging_fps * [-5 -1 0 5];
     fid = fopen(outname, 'w');
     for k = 1:length(reward_frames)
