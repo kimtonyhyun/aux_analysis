6ae93c0df01ccbe1687f2d15af7218eb3619b0e8
kimth <kimth@stanford.edu>
2020-10-20 15:29:01 -0700

    Skeleton WIP

--- +ctxstr/+dlc/compute_skeleton.m
+++ +ctxstr/+dlc/compute_skeleton.m
@@ -1,7 +1,5 @@
 function skeleton = compute_skeleton(dlc_data, bdata)
 
-num_dlc_frames = length(dlc_data.t);
-
 front_left = dlc_data.front_left(:,1:2);
 front_right = dlc_data.front_right(:,1:2);
 
@@ -23,9 +21,10 @@ us_times = bdata.us_times(bdata.lick_responses);
 num_trials = length(us_times);
 num_total_trials = length(bdata.lick_responses);
 
-% Compute skeleton parameters
+% Compute skeleton parameters. See definitions from 2020 Oct 20 diagram
 %------------------------------------------------------------
 
+% Angles
 body_vec = tail - nose; % Nose-to-tail vector
 alpha_n = 180/pi*atan2(body_vec(:,2), body_vec(:,1));
 
@@ -38,22 +37,27 @@ alpha_h = 180/pi*atan2(hind_limb_vec(:,2), hind_limb_vec(:,1));
 beta_f = alpha_f - alpha_n;
 beta_h = alpha_h - alpha_n;
 
-% This is an approximation to the "intersection" quantities d
-x_f = 1/2*(front_left(:,2) + front_right(:,2)) - nose(:,2);
-x_h = 1/2*(hind_left(:,2) + hind_right(:,2)) - nose(:,2);
+% Distances
+body_dir = normr(body_vec);
+
+front_vec = 1/2*(front_left + front_right) - nose;
+hind_vec = 1/2*(hind_left + hind_right) - nose;
+
+d_f = sum(front_vec .* body_dir,2);
+d_h = sum(hind_vec .* body_dir,2);
 
 skeleton.alpha_n = alpha_n;
 skeleton.alpha_f = alpha_f;
 skeleton.alpha_h = alpha_h;
 skeleton.beta_f = beta_f;
 skeleton.beta_h = beta_h;
-skeleton.x_f = x_f;
-skeleton.x_h = x_h;
+skeleton.d_f = d_f;
+skeleton.d_h = d_h;
 
 % Display results
 %------------------------------------------------------------
 
-ax1 = subplot(311);
+ax1 = subplot(411);
 
 yyaxis left;
 v_lims = tight_plot(velocity(:,1), velocity(:,2));
@@ -77,7 +81,7 @@ title(sprintf('%s (%d consumed out of %d total trials)',...
         dataset_name, num_trials, num_total_trials));
 set(ax1, 'TickLength', [0 0]);
 
-ax2 = subplot(312);
+ax2 = subplot(412);
 y_lims = tight_plot(dlc_data.t, alpha_n);
 hold on;
 plot_vertical_lines(movement_onset_times, y_lims, 'r:');
@@ -86,7 +90,7 @@ hold off;
 ylabel('Body angle, \alpha_n (degrees)');
 set(ax2, 'TickLength', [0 0]);
 
-ax3 = subplot(313);
+ax3 = subplot(413);
 yyaxis left;
 y_lims = [0 180];
 tight_plot(dlc_data.t, beta_f);
@@ -106,7 +110,21 @@ ylabel('Hind limb angle, \beta_h (degrees)');
 set(ax3, 'TickLength', [0 0]);
 set(ax3, 'YTick', [0 45 90 135 180]);
 
-linkaxes([ax1 ax2 ax3], 'x');
+ax4 = subplot(414);
+yyaxis left;
+y_lims = tight_plot(dlc_data.t, d_f);
+hold on;
+plot_vertical_lines(movement_onset_times, y_lims, 'r:');
+plot_vertical_lines(us_times, y_lims, 'b:');
+hold off;
+ylabel('Front limb position, d_f (px)');
+xlabel('Time (s)');
+
+yyaxis right;
+tight_plot(dlc_data.t, d_h);
+ylabel('Hind limb position, d_h (px)');
+
+linkaxes([ax1 ax2 ax3 ax4], 'x');
 zoom xon;
 
 end % compute_skeleton
