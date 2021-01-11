7e8a647f8a66c16ea9d57057810c75d21b0401d5
kimth <kimth@stanford.edu>
2021-01-10 16:57:34 -0800

    Avoid making 2 copies of 1P movie in memory

--- 1p2p/preproc_1p2p.m
+++ 1p2p/preproc_1p2p.m
@@ -8,13 +8,11 @@ dataset_name = dirname;
 filename_1p = get_most_recent_file('', '*.hdf5');
 filename_2p = get_most_recent_file('', '*.tif');
 
-% M_1p = h5read(filename_1p, '/1');
-M_1p = load_movie(filename_1p);
 M_2p = load_scanimage_tif(filename_2p);
 
 %%
 
-F_1p = compute_fluorescence_stats(M_1p);
+F_1p = compute_fluorescence_stats(filename_1p);
 F_2p = compute_fluorescence_stats(M_2p);
 
 ax1 = subplot(211);
@@ -30,33 +28,33 @@ xlim([0 1000]);
 
 %% Decide number of frames to chop
 
-max_frames = min(size(M_1p,3), size(M_2p,3));
-keep_frames = 201:18000;
+max_frames = min(size(F_1p,1), size(F_2p,1)); % Sometimes there is 1 extra frame
+keep_frames = 201:max_frames;
 
-M_1p_chopped = M_1p(:,:,keep_frames);
-M_2p_chopped = M_2p(:,:,keep_frames);
+M_1p = load_movie_from_hdf5(filename_1p, keep_frames([1 end]));
+M_2p = M_2p(:,:,keep_frames);
 
 %% Crop the 2P movie, to remove chopper and other edge effects
 
-% view_movie(M_2p_chopped, 'clim', [0 6000]);
+% view_movie(M_2p, 'clim', [0 6000]);
 
 horiz_trim = 20;
-keep_cols_2p = (1+horiz_trim):(size(M_2p_chopped,2)-horiz_trim);
-keep_rows_2p = 40:460;
+keep_cols_2p = (1+horiz_trim):(size(M_2p,2)-horiz_trim);
+keep_rows_2p = 50:460;
 
-M_2p_chopped = M_2p_chopped(keep_rows_2p, keep_cols_2p, :);
+M_2p = M_2p(keep_rows_2p, keep_cols_2p, :);
 
 % Rescale 2P zero
-F_2p_chopped = compute_fluorescence_stats(M_2p_chopped);
-M_2p_chopped = M_2p_chopped - int16(mean(F_2p_chopped(:,1)));
+F_2p_chopped = compute_fluorescence_stats(M_2p);
+M_2p = M_2p - int16(mean(F_2p_chopped(:,1)));
 
 %% Save data
 
 savename_1p = sprintf('%s-1P.hdf5', dataset_name);
 savename_2p = sprintf('%s-2P.hdf5', dataset_name);
 
-save_movie_to_hdf5(M_1p_chopped, savename_1p);
-save_movie_to_hdf5(M_2p_chopped, savename_2p);
+save_movie_to_hdf5(M_1p, savename_1p);
+save_movie_to_hdf5(M_2p, savename_2p);
 
 save('preproc_1p2p', 'dataset_name', 'keep_frames', 'keep_cols_2p', 'keep_rows_2p');
 
