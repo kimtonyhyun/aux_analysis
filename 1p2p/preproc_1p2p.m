b8bee7bc2f3164b42bd359026ebb40609d6870a2
kimth <kimth@stanford.edu>
2021-03-05 16:55:52 -0800

    Save individual planes to file

--- 1p2p/preproc_1p2p.m
+++ 1p2p/preproc_1p2p.m
@@ -29,7 +29,7 @@ xlim([0 1000]);
 %% Decide number of frames to chop
 
 max_frames = min(size(F_1p,1), size(F_2p,1)); % Sometimes there is 1 extra frame
-keep_frames = 201:max_frames;
+keep_frames = 241:max_frames;
 
 M_1p = load_movie_from_hdf5(filename_1p, keep_frames([1 end]));
 M_2p = M_2p(:,:,keep_frames);
@@ -38,9 +38,9 @@ M_2p = M_2p(:,:,keep_frames);
 
 % view_movie(M_2p, 'clim', [0 6000]);
 
-horiz_trim = 20;
+horiz_trim = 15;
 keep_cols_2p = (1+horiz_trim):(size(M_2p,2)-horiz_trim);
-keep_rows_2p = 50:450;
+keep_rows_2p = 50:512;
 
 M_2p = M_2p(keep_rows_2p, keep_cols_2p, :);
 
@@ -69,10 +69,17 @@ movefile(savename_2p, '2P');
 movie_filename = get_most_recent_file('', '*.hdf5');
 M0 = load_movie(movie_filename);
 
-num_planes = 5;
+[~, movie_stem] = fileparts(movie_filename);
+
+num_planes = 6;
 M = cell(num_planes,1);
 for k = 1:num_planes
     M{k} = M0(:,:,k:num_planes:end);
+    
+    sl_dirname = sprintf('sl%d', k);
+    mkdir(sl_dirname);   
+    sl_name = sprintf('%s/%s-sl%d.hdf5', sl_dirname, movie_stem, k);
+    save_movie_to_hdf5(M{k}, sl_name);
 end
 
 %% Cell extraction: EXTRACT
@@ -93,18 +100,18 @@ cprintf('Blue', '%s: Running EXTRACT on "%s"...\n', datestr(now), movie_filename
 
 config = get_defaults([]);
 config.preprocess = 0;
-config.cellfind_min_snr = 1.1;
+config.cellfind_min_snr = 1;
 
 switch dirname
     case '1P'
-        config.num_partitions_x = 3;
-        config.num_partitions_y = 3;
+        config.num_partitions_x = 1;
+        config.num_partitions_y = 1;
         config.avg_cell_radius = 15;
         
     otherwise % Assume 2P
         config.num_partitions_x = 1;
         config.num_partitions_y = 1;
-        config.avg_cell_radius = 7;
+        config.avg_cell_radius = 10;
 end
 
 output = extractor(sprintf('%s:/Data/Images', movie_filename), config);
