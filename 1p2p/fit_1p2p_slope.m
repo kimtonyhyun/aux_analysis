88aac243e1d3789fe29ac41b101f28bce9beb02f
kimth <kimth@stanford.edu>
2021-05-13 22:23:42 -0700

    Compute fraction variance explained

--- 1p2p/fit_1p2p_slope.m
+++ 1p2p/fit_1p2p_slope.m
@@ -5,23 +5,40 @@ function [slope, info] = fit_1p2p_slope(tr2, tr1)
 
 % Fit a polynomial curve to the 1P/2P scatter plot
 order = 2;
-mask = (tr2 > 3); % The cloud of points near the origin skews the fit
+zsc_thresh = 3;
+
+% Exclude the baseline portions from fitting
+mask1 = (tr1 > zsc_thresh);
+mask2 = (tr2 > zsc_thresh);
+mask = mask1 | mask2;
+
+% mask = true(length(tr1),1); % Debug: Use all samples for fit
+
 if sum(mask) <= order
     slope = [];
     info = [];
 else
     p = polyfitZero(tr2(mask), tr1(mask), order);
+%     p = polyfit(tr2(mask), tr1(mask), order);
     x = linspace(min(tr2), max(tr2));
     y = polyval(p, x);
 
+    % Fraction of variance explained. Note that if polyfitZero is used, the
+    % fve can be negative
+    tr1_fit = polyval(p, tr2);
+    fve = 1 - var(tr1(mask)-tr1_fit(mask))/var(tr1(mask));
+    
     % Evaluate slope of fit
     pd = polyder(p);
     xd = 1/3*max(tr2); % Evaluate at 33.3% of the dynamic range
     slope = polyval(pd, 1/3*max(tr2));
 
     % Package for output
+    info.fit.zsc_thresh = zsc_thresh;
+    info.fit.mask = mask;
     info.fit.order = order;
     info.fit.x = x;
     info.fit.y = y;
+    info.fit.fve = fve;
     info.xd = xd; % Evaluation point for the derivative
 end
