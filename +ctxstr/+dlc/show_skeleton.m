61694d7e48749918fef622908aad45162d3830d8
kimth <kimth@stanford.edu>
2021-08-19 08:53:14 -0700

    Option to show trial indices

--- +ctxstr/+dlc/show_skeleton.m
+++ +ctxstr/+dlc/show_skeleton.m
@@ -1,5 +1,7 @@
 function show_skeleton(path_to_behavior, path_to_skeleton)
 
+show_trial_idx = true;
+
 % Defaults
 %------------------------------------------------------------
 if ~exist('path_to_behavior', 'var')
@@ -32,7 +34,6 @@ position = cat(1, behavior.position.by_trial{:}); % Wrapped position
 movement_onset_times = behavior.movement_onset_times;
 us_times = behavior.us_times;
 num_trials = length(us_times);
-num_total_trials = length(behavior.lick_responses);
 
 % Display results
 %------------------------------------------------------------
@@ -44,35 +45,35 @@ sp = @(m,n,p) subtightplot(m, n, p, 0.05, 0.05, 0.05); % Gap, Margin-X, Margin-Y
 ax1 = sp(4,1,1);
 
 yyaxis left;
-v_lims = tight_plot(velocity(:,1), velocity(:,2));
+v_lims = tight_plot(velocity(:,1), velocity(:,2), 'HitTest', 'off');
 ylabel('Velocity (cm/s)');
 hold on;
 % plot(x_lims, [0 0], 'k--');
 y_pos = v_lims(1) + 0.95*diff(v_lims);
-plot(behavior.lick_times,...
-     y_pos*ones(size(behavior.lick_times)), 'b.');
+plot(behavior.lick_times, y_pos*ones(size(behavior.lick_times)),...
+    'b.', 'HitTest', 'off');
 hold off;
 
 yyaxis right;
-p_lims = tight_plot(position(:,1), position(:,2));
+p_lims = tight_plot(position(:,1), position(:,2), 'HitTest', 'off');
 ylabel('Position (encoder count)');
 hold on;
 plot_rectangles(behavior.opto_periods, p_lims);
-plot_vertical_lines(movement_onset_times, p_lims, 'r:');
-plot_vertical_lines(us_times, p_lims, 'b:');
+plot_vertical_lines(movement_onset_times, p_lims, 'r:', 'HitTest', 'off');
+plot_vertical_lines(us_times, p_lims, 'b:', 'HitTest', 'off');
 hold off;
 xlim(x_lims);
 title(sprintf('%s (%d consumed out of %d total trials)',...
-        dataset_name, num_trials, num_total_trials));
+        dataset_name, sum(behavior.lick_responses), num_trials));
 set(ax1, 'TickLength', [0 0]);
 
 % Nose angle
 ax2 = sp(4,1,2);
-y_lims = tight_plot(t, alpha_n);
+y_lims = tight_plot(t, alpha_n, 'HitTest', 'off');
 hold on;
 plot_rectangles(behavior.opto_periods, y_lims);
-plot_vertical_lines(movement_onset_times, y_lims, 'r:');
-plot_vertical_lines(us_times, y_lims, 'b:');
+plot_vertical_lines(movement_onset_times, y_lims, 'r:', 'HitTest', 'off');
+plot_vertical_lines(us_times, y_lims, 'b:', 'HitTest', 'off');
 hold off;
 ylabel('Body angle, \alpha_n (degrees)');
 set(ax2, 'TickLength', [0 0]);
@@ -81,19 +82,19 @@ set(ax2, 'TickLength', [0 0]);
 ax3 = sp(4,1,3);
 yyaxis left;
 y_lims = [0 180];
-tight_plot(t, beta_f);
+tight_plot(t, beta_f, 'HitTest', 'off');
 hold on;
 plot_rectangles(behavior.opto_periods, y_lims);
 plot(t([1 end]), 90*[1 1], 'k--');
-plot_vertical_lines(movement_onset_times, y_lims, 'r:');
-plot_vertical_lines(us_times, y_lims, 'b:');
+plot_vertical_lines(movement_onset_times, y_lims, 'r:', 'HitTest', 'off');
+plot_vertical_lines(us_times, y_lims, 'b:', 'HitTest', 'off');
 hold off;
 ylim(y_lims);
 set(ax3, 'YTick', [0 45 90 135 180]);
 ylabel('Front limb angle, \beta_f (degrees)');
 
 yyaxis right;
-tight_plot(t, beta_h);
+tight_plot(t, beta_h, 'HitTest', 'off');
 ylim(y_lims);
 ylabel('Hind limb angle, \beta_h (degrees)');
 set(ax3, 'TickLength', [0 0]);
@@ -102,21 +103,29 @@ set(ax3, 'YTick', [0 45 90 135 180]);
 % Limb positions
 ax4 = sp(4,1,4);
 yyaxis left;
-y_lims = tight_plot(t, d_f);
+y_lims = tight_plot(t, d_f, 'HitTest', 'off');
 hold on;
 plot_rectangles(behavior.opto_periods, y_lims);
-plot_vertical_lines(movement_onset_times, y_lims, 'r:');
-plot_vertical_lines(us_times, y_lims, 'b:');
+plot_vertical_lines(movement_onset_times, y_lims, 'r:', 'HitTest', 'off');
+plot_vertical_lines(us_times, y_lims, 'b:', 'HitTest', 'off');
 hold off;
 ylabel('Front limb position, d_f (px)');
 xlabel('Time (s)');
-
 yyaxis right;
-tight_plot(t, d_h);
+tight_plot(t, d_h, 'HitTest', 'off');
 ylabel('Hind limb position, d_h (px)');
+set(ax4, 'TickLength', [0 0]);
 
 linkaxes([ax1 ax2 ax3 ax4], 'x');
 zoom xon;
+
+if show_trial_idx
+    for a = [ax1 ax2 ax3 ax4]
+        set(a, 'XTick', behavior.us_times(1:5:end));
+        set(a, 'XTickLabel', 1:5:num_trials);
+    end
+    xlabel('Reward index');
+end
 
 end
 
