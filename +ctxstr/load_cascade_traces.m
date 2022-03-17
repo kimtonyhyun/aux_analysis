cd140cfa1ac8b377e3e507da693557f1d9b8171c
kimth <kimth@stanford.edu>
2022-03-17 10:55:22 -0700

    Filter tdt inds

--- +ctxstr/load_cascade_traces.m
+++ +ctxstr/load_cascade_traces.m
@@ -2,11 +2,25 @@ function [traces, tdt] = load_cascade_traces(path_to_imdata, fps)
 
 data = load(get_most_recent_file(path_to_imdata, 'cascade_*.mat'), 'spike_probs');
 class = load_cell_class(path_to_imdata);
+num_all_sources = length(class);
 
 traces = fps * data.spike_probs';  % Convert to spike rates (Hz); [Cells x Time]
 traces = traces(class,:); % Return only sources classified to be cells
 
-tdt = load_tdt(path_to_imdata); % FIXME: Filtering by class changes cell indices
+tdt = load_tdt(path_to_imdata);
+if ~isempty(tdt)
+    % Below: Make cell indices consistent with 'traces', given that the latter
+    % only keeps sources that are classified to be cells
+    tdt_pos_cells = zeros(1, num_all_sources);
+    tdt_pos_cells(tdt.pos) = 1;
+    tdt_pos_cells = tdt_pos_cells(class);
+    tdt.pos = find(tdt_pos_cells);
+
+    tdt_neg_cells = zeros(1, num_all_sources);
+    tdt_neg_cells(tdt.neg) = 1;
+    tdt_neg_cells = tdt_neg_cells(class);
+    tdt.neg = find(tdt_neg_cells);
+end
 
 end
 
