458fca9f241e30c921ca348a053480913303abd7
kimth <kimth@stanford.edu>
2022-04-16 20:06:49 -0700

    Relabel Pearson corr r

--- +ctxstr/+analysis/load_data.m
+++ +ctxstr/+analysis/load_data.m
@@ -126,3 +126,97 @@ for k = 1:num_str_cells
     print('-dpng', sprintf('%s-str_cell-%03d_raster.png', dataset_name, k));
 %     pause;
 end
+
+%% Visualization #4: Show basic behavior + some neurons (WIP)
+
+sp = @(m,n,p) subtightplot(m, n, p, [0.01 0.05], 0.04, 0.04); % Gap, Margin-X, Margin-Y
+
+trials_to_show = st_trial_inds;
+ctx_inds_to_show = [11 15 18];
+str_inds_to_show = [12 22 28];
+
+num_ctx_to_show = length(ctx_inds_to_show);
+num_str_to_show = length(str_inds_to_show);
+
+num_rows = 1+num_ctx_to_show+num_str_to_show;
+h_axes = zeros(num_rows, 1);
+
+clf;
+h_axes(1) = sp(num_rows,1,1);
+yyaxis left;
+hold on;
+for k = trials_to_show
+    vel = trials(k).velocity;
+    plot(vel(:,1), vel(:,2), '-');
+end
+hold off;
+ylim([-5 45]);
+ylabel('Velocity (cm/s)');
+yyaxis right;
+y_lims = [0 session.behavior.position.us_threshold];
+hold on;
+for k = trials_to_show
+    trial = trials(k);
+    
+    plot(trial.position(:,1), trial.position(:,2), '-');
+    plot(trial.lick_times, 0.95*y_lims(2)*ones(size(trial.lick_times)), 'b.');
+    
+    plot_vertical_lines([trial.start_time trial.us_time], y_lims, 'b:');
+    plot_vertical_lines(trial.motion.onsets, y_lims, 'r:');
+end
+ylim(y_lims);
+ylabel('Position');
+set(gca, 'TickLength', [0 0]);
+title(dataset_name);
+
+y_lims = [-0.15 1.15];
+for i = 1:num_ctx_to_show
+    h_axes(1+i) = sp(num_rows, 1, 1+i);
+    ctx_idx = ctx_inds_to_show(i);
+    
+    hold on;
+    for k = trials_to_show
+        trial = trials(k);
+        trial_time = [trial.start_time trial.us_time];
+    
+        [ctx_traces_k, ctx_times_k] = ctxstr.core.get_traces_by_time(ctx, trial_time);
+        plot(ctx_times_k, ctx_traces_k(ctx_idx,:), 'k-');
+    
+        plot_vertical_lines([trial.start_time trial.us_time], y_lims, 'b:');
+        plot_vertical_lines(trial.motion.onsets, y_lims, 'r:');
+    end
+    hold off;
+    ylim(y_lims);
+    ylabel(sprintf('Ctx cell #=%d', ctx_idx));
+end
+
+for j = 1:num_str_to_show
+    h_axes(1+num_ctx_to_show+j) = sp(num_rows, 1, 1+num_ctx_to_show+j);
+    str_idx = str_inds_to_show(j);
+    
+    hold on;
+    for k = trials_to_show
+        trial = trials(k);
+        trial_time = [trial.start_time trial.us_time];
+    
+        [str_traces_k, str_times_k] = ctxstr.core.get_traces_by_time(str, trial_time);
+        plot(str_times_k, str_traces_k(str_idx,:), 'm-');
+    
+        plot_vertical_lines([trial.start_time trial.us_time], y_lims, 'b:');
+        plot_vertical_lines(trial.motion.onsets, y_lims, 'r:');
+    end
+    hold off;
+    ylim(y_lims);
+    ylabel(sprintf('Str cell #=%d', str_idx));
+end
+xlabel('Trial index');
+
+linkaxes(h_axes, 'x');
+set(h_axes, 'TickLength', [0.001 0]);
+set(h_axes, 'XTick', [trials(trials_to_show).start_time]);
+set(h_axes(1:end-1), 'XTickLabel', []);
+set(h_axes(end), 'XTickLabel', trials_to_show);
+set(h_axes(2:end), 'YTick', [0 1]);
+
+xlim([trials(trials_to_show(1)).start_time trials(trials_to_show(end)).us_time]);
+zoom xon;
