e4a34bdd2aec3948a7a60ee6ad73f3719f0be5cd
kimth <kimth@stanford.edu>
2022-04-17 10:02:49 -0700

    Save corr data

--- +ctxstr/+analysis/compute_correlations.m
+++ +ctxstr/+analysis/compute_correlations.m
@@ -1,12 +1,12 @@
 %% Resample traces with a common timebase, then compute pairwise correlations
 
-trials_to_use = st_trial_inds;
+trials_for_corr = st_trial_inds;
 
 resampled_ctx_traces = cell(1, num_all_trials);
 resampled_str_traces = cell(1, num_all_trials);
 common_time = cell(1, num_all_trials);
 
-for k = trials_to_use
+for k = trials_for_corr
     trial = trials(k);
     trial_time = [trial.start_time trial.us_time];
     
@@ -14,7 +14,7 @@ for k = trials_to_use
     [str_traces_k, str_times_k] = ctxstr.core.get_traces_by_time(str, trial_time);
     
     if any(isnan(ctx_traces_k(:))) || any(isnan(str_traces_k(:)))
-        trials_to_use = setdiff(trials_to_use, k);
+        trials_for_corr = setdiff(trials_for_corr, k);
         fprintf('Omitting Trial %d from correlation calculation due to NaNs\n', k);
     else
         [resampled_ctx_traces{k}, resampled_str_traces{k}, common_time{k}] = ctxstr.core.resample_ctxstr_traces(...
@@ -34,6 +34,13 @@ cont_str_traces = cell2mat(resampled_str_traces);
 C_ctx = corr(cont_ctx_traces');
 C_str = corr(cont_str_traces');
 C_ctxstr = corr(cont_ctx_traces', cont_str_traces');
+
+%% Save correlation results
+
+save('corrdata.mat', 'dataset_name', 'trials', 'trials_for_corr',...
+        'common_time', 'resampled_ctx_traces', 'resampled_str_traces',...
+        'ctx_info', 'str_info',...
+        'C_ctx', 'C_str', 'C_ctxstr');
 
 %% Visualization #1: Correlation matrices and distribution of correlation values
 
