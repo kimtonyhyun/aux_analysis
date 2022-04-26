a36946b78a21927ac303c0e7ef4112cde683d4c9
kimth <kimth@stanford.edu>
2022-04-26 15:13:11 -0700

    Assign events to frames based on temporal proximity

--- +ctxstr/+core/assign_events_to_frames.m
+++ +ctxstr/+core/assign_events_to_frames.m
@@ -1,7 +1,9 @@
 function has_event = assign_events_to_frames(event_times, frame_times)
-% Compute a Boolean trace `has_events' with the same number of frames as
-% `frame_times` where `has_event(k)` is 1 if there is an event that occurs
-% between the start of the k-th and (k+1)-th frame.
+% For each event that occurs between the k-th and (k+1)-th frame, assign
+% the event to either has_event(k) or has_event(k+1), depending on
+% whichever is closer in time.
+%
+% See also 'assign_edges_to_frames.m'.
 
 num_events = length(event_times);
 num_frames = length(frame_times);
@@ -11,11 +13,21 @@ has_event = false(num_frames, 1);
 first_assigned_event = find(event_times > frame_times(1), 1);
 
 event_idx = first_assigned_event;
+event_time = event_times(event_idx);
 for k = 1:num_frames-1
-    if (event_times(event_idx) < frame_times(k+1))
-        has_event(k) = true;
+    if (event_time < frame_times(k+1)) % event_time is between frame_times(k) and frame_times(k+1)
+
+        % Determine whether the k-th or (k+1)-th frame is closer in time
+        if abs(event_time - frame_times(k)) < abs(event_time - frame_times(k+1))
+            has_event(k) = true;
+        else
+            has_event(k+1) = true;
+        end
+        
+        % Move on to next event
         if (event_idx < num_events)
             event_idx = event_idx + 1;
+            event_time = event_times(event_idx);
         else
             break;
         end
