a9da77844c2848efa27a37bcf8b616e5a46a0df8
kimth <kimth@stanford.edu>
2022-05-10 10:33:28 -0700

    Make AVI save similar to view_movie

--- io/save_movie_to_avi.m
+++ io/save_movie_to_avi.m
@@ -1,4 +1,4 @@
-function save_movie_to_avi(M, scale)
+function save_movie_to_avi(M, varargin)
 % Save movie matrix M into an uncompressed AVI file for presentation
 % purposes. Consider compressing the output AVI file into MP4 using
 % external software such as Handbrake.
@@ -9,18 +9,33 @@ function save_movie_to_avi(M, scale)
 
 % Default parameters:
 frame_rate = 30; % Hz. Going below 30 Hz usually doesn't look good.
+movie_clim = [];
+use_outline = 0;
+
+for k = 1:length(varargin)
+    vararg = varargin{k};
+    if ischar(vararg)
+        switch lower(vararg)
+            case 'clim'
+                movie_clim = varargin{k+1};
+            case 'boundary'
+                use_outline = 1;
+                ds = varargin{k+1};
+        end
+    end
+end
 
 timestamp = datestr(now, 'yymmdd-HHMMSS');
 output_name = sprintf('out_%s.avi', timestamp);
 
-if ~exist('scale', 'var') % Scaling not provided
+if isempty(movie_clim) % Scaling not provided
     switch class(M)
         case {'uint8'}
-            scale = [0 255];
+            movie_clim = [0 255];
         case {'uint16'}
-            scale = [0 0.9*max(M(:))];
+            movie_clim = [0 0.9*max(M(:))];
         case {'single'}
-            scale = compute_movie_scale(M);
+            movie_clim = compute_movie_scale(M);
     end
 end
 
@@ -32,8 +47,13 @@ nd = ndims(M);
 switch nd
     case 3 % [height x width x num_frames]       
         get_frame = @(k) M(:,:,k);
-        h = imagesc(get_frame(1), scale);
+        h = imagesc(get_frame(1), movie_clim);
         colormap gray;
+        if use_outline
+            hold on;
+            plot_boundaries(ds, 'Color', 'g');
+        end
+
     case 4 % [height x width x RGB x num_frames]
         get_frame = @(k) M(:,:,:,k);
         h = image(get_frame(1));
