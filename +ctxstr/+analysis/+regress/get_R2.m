2a54b4f999a8c2e5ee67e172be269b343b91ef89
kimth <kimth@stanford.edu>
2022-12-27 10:55:47 -0800

    Return both value and error

--- +ctxstr/+analysis/+regress/get_R2.m
+++ +ctxstr/+analysis/+regress/get_R2.m
@@ -1,8 +1,10 @@
-function R2_val = get_R2(reg, brain_area, cell_idx, model_no)
+function [R2_val, R2_err] = get_R2(reg, brain_area, cell_idx, model_no)
 
 switch brain_area
     case 'ctx'
         R2_val = reg.ctx_fit.results.R2(cell_idx, model_no);
+        R2_err = reg.ctx_fit.results.error(cell_idx, model_no);
     case 'str'
         R2_val = reg.str_fit.results.R2(cell_idx, model_no);
+        R2_err = reg.str_fit.results.error(cell_idx, model_no);
 end
